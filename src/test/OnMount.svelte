4be90f3f3990538c24123daa6bfbaeae57e61715
Fractal <55504972+FractalHQ@users.noreply.github.com>
2021-09-14 16:50:30 -0400

    [style] add inline code to OnMount description

--- src/test/OnMount.svelte
+++ src/test/OnMount.svelte
@@ -25,7 +25,14 @@
 
 <Item title="OnMount" type="component" {example}>
 	<div slot="description">
-		Mounts an element on Svelte's `onMount` lifecycle hook.
+		Mounts an element on Svelte's <span
+			class="token language-javascript"
+			style="display: inline; padding: 2px 5px; font-style: normal; font-size: 0.8rem; margin: 0 3px;"
+			>onMount</span
+		>
+		<a href="https://svelte.dev/docs#onMount" target="_blank">
+			lifecycle hook
+		</a>.
 	</div>
 
 	<button on:click={reload}>window.location.reload()</button>
@@ -50,5 +57,7 @@
 		background: var(--text-a);
 		color: var(--bg-a);
 		border-radius: var(--border-radius);
+		font-size: 0.9rem;
+		padding: 3px 5px;
 	}
 </style>
