4803a21614a7ee3bb8de3ee57962743dadc5ade5
Fractal <55504972+FractalHQ@users.noreply.github.com>
2021-09-14 16:48:38 -0400

    [chore] extract styles imports to __layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '@fontsource/inconsolata'
+	import 'greset/css/greset.css'
+	import '@fontsource/alata'
+	import '../prism.css'
+	import '../app.scss'
+</script>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,51 @@
-<script context="module">
-	export const ssr = false
-</script>
-
 <script lang="ts">
 	import ClickOutside from '../test/ClickOutside.svelte'
 	import Browser from '../test/Browser.svelte'
 	import OnMount from '../test/OnMount.svelte'
+	import Nav from '../test/Nav.svelte'
 	import Dev from '../test/Dev.svelte'
-	import 'greset/css/greset.css'
-	import '@fontsource/alata'
-	import '../prism.css'
-	import '../app.scss'
 </script>
+
+<header>
+	<h1>fractils</h1>
+	<p class="description">
+		<span style="font-style: normal !important; font-size: 0.9rem;"
+			>npm i -D fractils</span
+		>
+	</p>
+</header>
+
+<Nav />
+
+<h2 id="env">env</h2>
 
 <Dev />
 
 <Browser />
 
+<h2 id="actions">actions</h2>
+
 <ClickOutside />
 
+<h2 id="components">components</h2>
+
 <OnMount />
+
+<h2 id="utils">utils</h2>
+
+<style>
+	header {
+		color: white;
+		padding-top: 0.5rem;
+	}
+	h1,
+	h2,
+	p {
+		padding: 0 1rem;
+		color: white;
+	}
+	h2 {
+		text-align: center;
+		margin-bottom: -2rem;
+	}
+</style>
