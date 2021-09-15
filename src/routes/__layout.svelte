fca250918e97a81238724755b3d08e1632f63b18
Fractal <55504972+FractalHQ@users.noreply.github.com>
2021-09-15 18:20:56 -0400

    [fix] font load jank

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,8 @@
 <script>
 	import 'greset/css/greset.css'
 
-	import '@fontsource/inconsolata'
-	import '@fontsource/alata'
+	// import '@fontsource/inconsolata'
+	// import '@fontsource/alata'
 
 	import '../prism.css'
 	import '../app.scss'
