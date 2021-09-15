7377927a96b1d08708f36d0e57b90b45289ea6f8
Fractal <55504972+FractalHQ@users.noreply.github.com>
2021-09-15 15:37:15 -0400

    Updates

---
+++ app.js
@@ -0,0 +1,104 @@
+import { respond } from '@sveltejs/kit/ssr';
+import root from './generated/root.svelte';
+import { set_paths, assets } from './runtime/paths.js';
+import { set_prerendering } from './runtime/env.js';
+import * as user_hooks from "./hooks.js";
+
+const template = ({ head, body }) => "<!DOCTYPE html>\n<html lang=\"en\">\n\t<head>\n\t\t<meta charset=\"utf-8\" />\n\t\t<link rel=\"icon\" href=\"/favicon.png\" />\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n\t\t" + head + "\n\t</head>\n\t<body>\n\t\t<div id=\"svelte\">" + body + "</div>\n\t</body>\n</html>\n";
+
+let options = null;
+
+const default_settings = { paths: {"base":"","assets":""} };
+
+// allow paths to be overridden in svelte-kit preview
+// and in prerendering
+export function init(settings = default_settings) {
+	set_paths(settings.paths);
+	set_prerendering(settings.prerendering || false);
+
+	const hooks = get_hooks(user_hooks);
+
+	options = {
+		amp: false,
+		dev: false,
+		entry: {
+			file: assets + "/_app/start-4f6800a3.js",
+			css: [assets + "/_app/assets/start-61d1577b.css"],
+			js: [assets + "/_app/start-4f6800a3.js",assets + "/_app/chunks/vendor-6f4202fa.js",assets + "/_app/chunks/preload-helper-ec9aa979.js"]
+		},
+		fetched: undefined,
+		floc: false,
+		get_component_path: id => assets + "/_app/" + entry_lookup[id],
+		get_stack: error => String(error), // for security
+		handle_error: (error, request) => {
+			hooks.handleError({ error, request });
+			error.stack = options.get_stack(error);
+		},
+		hooks,
+		hydrate: true,
+		initiator: undefined,
+		load_component,
+		manifest,
+		paths: settings.paths,
+		prerender: true,
+		read: settings.read,
+		root,
+		service_worker: null,
+		router: true,
+		ssr: true,
+		target: "#svelte",
+		template,
+		trailing_slash: "never"
+	};
+}
+
+const d = decodeURIComponent;
+const empty = () => ({});
+
+const manifest = {
+	assets: [{"file":"favicon.png","size":8265,"type":"image/png"}],
+	layout: "src/routes/__layout.svelte",
+	error: ".svelte-kit/build/components/error.svelte",
+	routes: [
+		{
+						type: 'page',
+						pattern: /^\/$/,
+						params: empty,
+						a: ["src/routes/__layout.svelte", "src/routes/index.svelte"],
+						b: [".svelte-kit/build/components/error.svelte"]
+					}
+	]
+};
+
+// this looks redundant, but the indirection allows us to access
+// named imports without triggering Rollup's missing import detection
+const get_hooks = hooks => ({
+	getSession: hooks.getSession || (() => ({})),
+	handle: hooks.handle || (({ request, resolve }) => resolve(request)),
+	handleError: hooks.handleError || (({ error }) => console.error(error.stack)),
+	externalFetch: hooks.externalFetch || fetch
+});
+
+const module_lookup = {
+	"src/routes/__layout.svelte": () => import("../../src/routes/__layout.svelte"),".svelte-kit/build/components/error.svelte": () => import("./components/error.svelte"),"src/routes/index.svelte": () => import("../../src/routes/index.svelte")
+};
+
+const metadata_lookup = {"src/routes/__layout.svelte":{"entry":"pages/__layout.svelte-d07978ca.js","css":["assets/pages/__layout.svelte-c59c63fc.css"],"js":["pages/__layout.svelte-d07978ca.js","chunks/vendor-6f4202fa.js"],"styles":[]},".svelte-kit/build/components/error.svelte":{"entry":"error.svelte-28b350df.js","css":[],"js":["error.svelte-28b350df.js","chunks/vendor-6f4202fa.js"],"styles":[]},"src/routes/index.svelte":{"entry":"pages/index.svelte-68a4f668.js","css":["assets/pages/index.svelte-46a607a8.css"],"js":["pages/index.svelte-68a4f668.js","chunks/vendor-6f4202fa.js","chunks/preload-helper-ec9aa979.js"],"styles":[]}};
+
+async function load_component(file) {
+	const { entry, css, js, styles } = metadata_lookup[file];
+	return {
+		module: await module_lookup[file](),
+		entry: assets + "/_app/" + entry,
+		css: css.map(dep => assets + "/_app/" + dep),
+		js: js.map(dep => assets + "/_app/" + dep),
+		styles
+	};
+}
+
+export function render(request, {
+	prerender
+} = {}) {
+	const host = request.headers["host"];
+	return respond({ ...request, host }, options, { prerender });
+}
