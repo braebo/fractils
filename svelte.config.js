3b1e59e9ab423528ee8e43608a64038edadf23d4
Fractal <55504972+FractalHQ@users.noreply.github.com>
2021-09-16 14:32:00 -0400

    [chore] remove log

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,6 @@ import vercel from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
 import { resolve } from 'path'
 
-console.log(resolve('./src/test'))
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [preprocess()],
