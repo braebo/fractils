a7a8b858ed59aff68ed2236d7e51786a49859250
Fractal <55504972+FractalHQ@users.noreply.github.com>
2021-09-16 14:32:31 -0400

    [fix] bug in scroll tracker

--- src/test/Nav.svelte
+++ src/test/Nav.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
+	import { onMount, tick } from 'svelte'
 
 	const paths = ['env', 'actions', 'components', 'utils']
 	let els: HTMLElement[] | [] = []
-
-	onMount(async () => {
+	function grabEls() {
+		els = []
 		paths.forEach((path) => {
 			const el = document.getElementById(`${path}`)
 			if (el) {
@@ -12,15 +12,36 @@
 			}
 		})
 		trackScroll()
+	}
+
+	onMount(() => {
+		grabEls()
 	})
 
-	let active = 0
-	function trackScroll() {
-		els.forEach((el, i) => {
-			if (el.getBoundingClientRect().top < 200) {
-				active = i
-			}
-		})
+	$: active = 0
+	let disableTracker = false
+	async function trackScroll() {
+		await tick()
+		if (!disableTracker) {
+			els.forEach(async (el, i) => {
+				const { top, width } = el.getBoundingClientRect()
+				if (width === 0) grabEls() // fixes weird bug where rects are all 0
+				if (top < 200) {
+					active = i
+				}
+			})
+		}
+	}
+
+	let timer: ReturnType<typeof setTimeout> | null = null
+	function handleClick(i: number) {
+		if (timer) clearTimeout(timer)
+		disableTracker = true
+		active = i
+		timer = setTimeout(() => {
+			disableTracker = false
+			grabEls()
+		}, 600)
 	}
 </script>
 
@@ -28,7 +49,10 @@
 
 <ul>
 	{#each paths as path, i}
-		<li class:active={active === i}>
+		<li
+			class:active={active === i}
+			on:click|capture={(e) => handleClick(i)}
+		>
 			<a href="#{path}">{path}</a>
 		</li>
 	{/each}
@@ -42,35 +66,41 @@
 		top: 40vh;
 		left: 1rem;
 		gap: 1rem;
+
 		color: var(--bg-a);
 	}
 
 	li {
 		position: relative;
+
 		list-style-type: none;
 	}
 
 	a,
 	li {
 		text-decoration: none;
+
 		color: var(--bg-a);
+
 		transition: 0.2s ease-in-out;
 	}
 
 	li:after {
-		content: '';
 		position: absolute;
-		left: -21px;
 		top: 9px;
+		left: -21px;
 
 		width: 10px;
 		height: 10px;
 
-		background: var(--color-primary);
 		border-radius: 100%;
 
-		transform: scale(0);
+		background: var(--color-primary);
+
+		content: '';
 		transition: 0.2s ease-out;
+
+		transform: scale(0);
 	}
 
 	.active,
