bb4cd8ca0693ff2b628088e8bf5b09e055bcb4cc
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-06-24 20:28:00 -0400

    [fix] export mobileThreshold and clickOutside types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,7 +4,7 @@ export { default as OnMount } from './components/OnMount.svelte'
 
 export { initTheme, toggleTheme, applyTheme, theme } from './theme'
 
-export { mobile, screenH, screenW, scrollY, mouse } from './stores'
+export { mobile, screenH, screenW, scrollY, mouse, mobileThreshold } from './stores'
 
 export { log, wait, localStorageStore, mapRange } from './utils'
 
@@ -14,3 +14,6 @@ export { clickOutside, visibility } from './actions'
 
 import './actions/visibility/events.d.ts'
 export type { VisibilityEventDetail, VisibilityEvent } from './actions/visibility/types'
+
+import './actions/clickOutside/events.d.ts'
+export type { ClickOutsideEventDetail, ClickOutsideEvent } from './actions/clickOutside/types'
--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,2 +1,2 @@
-export { mobile, screenH, screenW, scrollY, mouse } from './Device.svelte'
+export { mobile, screenH, screenW, scrollY, mouse, mobileThreshold } from './Device.svelte'
 export { default as device } from './Device.svelte'
