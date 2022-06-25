005239692f4015dc7e52afbb5ce9155c12336120
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-06-25 01:42:38 -0400

    [fix] duplicate export

---
+++ .changeset/curly-peas-sip.md
@@ -0,0 +1,5 @@
+---
+'fractils': patch
+---
+
+[fix] duplicate export
--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,2 +1,1 @@
 export { mobile, screenH, screenW, scrollY, mouse, mobileThreshold } from './Device.svelte'
-export { default as device } from './Device.svelte'
