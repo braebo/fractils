6f8de703e5d330254616cf3c5c14c197e40eff6f
Braden Wiggins <55504972+FractalHQ@users.noreply.github.com>
2023-08-20 23:31:48 -0400

    [feat] tree node inspector

---
+++ src/examples/_lib/NodeInspector.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { Node } from './Nav.svelte'
+
+	export let node: Node
+</script>
+
+<div class="nodes">
+	<div class:active={node.childActive}>{node.name}</div>
+	{#if node.children}
+		{#each node.children as child}
+			<svelte:self node={child} />
+		{/each}
+	{/if}
+</div>
+
+<style lang="scss">
+	.nodes {
+		position: sticky;
+		top: 5rem;
+
+		margin-left: 1rem;
+	}
+
+	div {
+		color: var(--fg-d);
+	}
+	.active {
+		color: var(--brand-a);
+	}
+</style>
