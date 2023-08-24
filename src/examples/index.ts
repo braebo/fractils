b56300aab75ef0a83440c4f1067a7262e827fa86
Braden Wiggins <55504972+FractalHQ@users.noreply.github.com>
2023-08-24 01:47:48 -0400

    [feat] reordering

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,23 +1,19 @@
 // import { entries } from '$lib/utils/object'
 
-import OnMount from './components/OnMount.svelte'
-import MacScrollbar from './components/MacScrollbar.svelte'
-import Device from './stores/Device.svelte'
-import LocalStorageStore from './utils/LocalStorageStore.svelte'
-import Log from './utils/Log.svelte'
-import Wait from './utils/Wait.svelte'
-import MapRange from './utils/MapRange.svelte'
-import Clamp from './utils/Clamp.svelte'
-import Theme from './utils/Theme.svelte'
-import ThemeToggle from './components/ThemeToggle.svelte'
-import ClickOutside from './actions/ClickOutside.svelte'
-import Visibility from './actions/Visibility.svelte'
+import Device from './stores/Device.svelte';
+import LocalStorageStore from './utils/LocalStorageStore.svelte';
+import Log from './utils/Log.svelte';
+import Wait from './utils/Wait.svelte';
+import MapRange from './utils/MapRange.svelte';
+import Clamp from './utils/Clamp.svelte';
+import Theme from './utils/Theme.svelte';
+import ThemeToggle from './components/ThemeToggle.svelte';
+import ClickOutside from './actions/ClickOutside.svelte';
+import Visibility from './actions/Visibility.svelte';
+import OnMount from './components/OnMount.svelte';
+import MacScrollbar from './components/MacScrollbar.svelte';
 
 export const EXAMPLES = {
-	components: {
-		OnMount,
-		MacScrollbar,
-	},
 	utils: {
 		device: Device,
 		localStorageStore: LocalStorageStore,
@@ -27,11 +23,15 @@ export const EXAMPLES = {
 		clamp: Clamp,
 	},
 	theme: {
-		theme: Theme,
 		ThemeToggle,
+		theme: Theme,
 	},
 	actions: {
 		clickOutside: ClickOutside,
 		visibility: Visibility,
 	},
-} as const
+	components: {
+		OnMount,
+		MacScrollbar,
+	},
+} as const;
