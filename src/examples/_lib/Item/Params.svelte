2c24259d8c740c0199d1eb017c7bd0ea87078e7d
Braden Wiggins <55504972+FractalHQ@users.noreply.github.com>
2023-08-24 01:49:37 -0400

    [fix] connector size calculator

--- src/examples/_lib/Item/Params.svelte
+++ src/examples/_lib/Item/Params.svelte
@@ -1,16 +1,36 @@
 <script context="module" lang="ts">
 	export interface Param {
-		type: string
-		title?: string
-		description?: string
-		children?: Param[]
+		id?: string;
+		type: string;
+		title?: string;
+		description?: string;
+		children?: Param[];
 	}
 </script>
 
 <script lang="ts">
-	export let params: Param[] = []
+	import { nanoid } from 'nanoid';
 
-	const hasChild = (p: Param) => p.children != undefined
+	export let params: Param[] = [];
+
+	let connectorStart = 0;
+	let connectorEnd = 0;
+	let height = 0;
+	let nodeHeight = 0;
+
+	const hasChild = (p: Param) => p.children != undefined;
+
+	function setConnectorLength(node: Element, options: { position: 'first' | 'last' }) {
+		nodeHeight = Math.max(node.clientHeight, nodeHeight);
+
+		if (options.position === 'first') {
+			connectorStart = node.getBoundingClientRect().top + nodeHeight;
+		} else {
+			connectorEnd = node.getBoundingClientRect().top + nodeHeight / 2;
+
+			height = Math.min(connectorEnd - connectorStart, nodeHeight);
+		}
+	}
 </script>
 
 <div class="params">
@@ -21,35 +41,57 @@
 					<div class="key type">
 						<div class="code">{p.type}</div>
 					</div>
+
 					<div class="key title">
 						{#if p.title}
 							<div>{p.title}</div>
 						{/if}
 					</div>
+
 					<div class="seperator" />
 				</div>
+
 				<div class="description">{@html p.description}</div>
 			</div>
+
 			{#if p.children}
 				{#each p.children as child, i}
+					{@const isFirst = i === 0}
+					{@const isLast = i + 1 === p.children.length}
+
 					<div class="param indented">
-						{#if i === 0}<div
+						{#if isFirst}
+							<div
+								use:setConnectorLength={{ position: 'first' }}
 								class="connector"
-								style="height: {20 *
-									(p.children.length > 1
-										? p.children.length * 1.95
-										: p.children.length)}px !important"
-							/>{/if}
+								style="height: {height}px !important"
+							/>
+						{/if}
+
 						<div class="row">
 							<div class="keys row">
 								<div class="key type">
-									<div class="code">{child.type}</div>
+									{#if isLast}
+										<div
+											class="code"
+											use:setConnectorLength={{ position: 'last' }}
+										>
+											{child.type}
+										</div>
+									{:else}
+										<div class="code">
+											{child.type}
+										</div>
+									{/if}
 								</div>
+
 								<div class="key title">
 									<div>{child.title}</div>
 								</div>
+
 								<div class="seperator" />
 							</div>
+
 							<div class="description">{@html child.description}</div>
 						</div>
 					</div>
@@ -61,7 +103,8 @@
 
 <style lang="scss">
 	.params {
-		padding-top: 0.5rem;
+		padding: 1.5rem 0;
+		font-family: var(--font-b);
 	}
 
 	.keys {
@@ -69,9 +112,21 @@
 		flex-shrink: 0;
 
 		width: var(--width, 140px);
+
+		font-family: var(--font-mono);
+	}
+
+	.keys.row {
+		flex-direction: row;
+		align-items: center;
+		height: fit-content;
 	}
 
 	.key {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
 		&,
 		.key div {
 			width: max-content;
@@ -100,11 +155,12 @@
 		&.title {
 			display: flex;
 			justify-content: flex-start;
+			align-items: center;
 
 			padding-left: 0.5rem;
 
 			font-size: 0.95rem;
-			font-weight: thin;
+			font-variation-settings: 'wght' 600;
 			transform: translateY(-1.5px);
 
 			color: var(--brand-b);
@@ -123,9 +179,21 @@
 		transform: translate(-4px, 2px) scaleX(0.5);
 	}
 
+	.description {
+		white-space: pre-wrap;
+		line-height: 1.4;
+	}
+
 	:global(.description em) {
 		opacity: 0.5;
-		font-family: var(--mono);
+		font-family: var(--font-mono);
+	}
+	:global(.description span.code) {
+		font-size: 0.85rem;
+		font-variation-settings: 'wght' 500;
+		background: var(--bg-a) !important;
+		padding: 3px 7px 4px 7px;
+		color: var(--fg-b) !important;
 	}
 
 	.col {
@@ -140,7 +208,7 @@
 	.row {
 		display: flex;
 		flex-direction: row;
-		align-items: center;
+		// align-items: center;
 	}
 
 	.indented {
@@ -154,7 +222,6 @@
 		left: -15px;
 		z-index: 1;
 
-		// height: 20px;
 		width: 10px;
 
 		border: 2px solid var(--fg-d);
