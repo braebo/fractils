62d615a32755e2894c812b052742056886ae9ee6
Braden Wiggins <55504972+FractalHQ@users.noreply.github.com>
2023-08-24 01:50:14 -0400

    [config] add shiki to build script

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,19 @@
-import vercel from '@sveltejs/adapter-vercel'
-import preprocess from 'svelte-preprocess'
-import { resolve } from 'path'
+import vercel from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [preprocess()],
-
 	kit: {
 		adapter: vercel(),
 		alias: {
 			$examples: resolve('./src/examples'),
 		},
 	},
-
 	vitePlugin: {
 		inspector: true,
 	},
-}
+};
 
-export default config
+export default config;
