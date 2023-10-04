a8cf6464e21b0e2a27cb3c1b6ef8732aee7e068f
Braden Wiggins <braden@fractal-hq.com>
2023-10-03 21:26:49 -0400

    [feat] chalk shorthands

---
+++ src/lib/utils/l.ts
@@ -0,0 +1,68 @@
+import { stringify } from './stringify'
+import c from 'chalk'
+
+// Ridiculously short color logging functions that
+// nobody should ever use. I just like them sometimes.
+
+/** console.log */
+export function l(...args: unknown[]) {
+	console.log(...args)
+}
+
+/** chalk.red */
+export function r(...args: unknown[]) {
+	return c.red(...args)
+}
+
+/** chalk.green */
+export function g(...args: unknown[]) {
+	return c.green(...args)
+}
+
+/** chalk.blue */
+export function b(...args: unknown[]) {
+	return c.blue(...args)
+}
+
+/** chalk.yellow */
+export function y(...args: unknown[]) {
+	return c.yellow(...args)
+}
+
+/** chalk.dim */
+export function dim(...args: unknown[]) {
+	return c.dim(...args)
+}
+
+/** chalk.bold */
+export function bd(...args: unknown[]) {
+	return c.bold(...args)
+}
+
+/**
+ * Logs an empty line.
+ */
+export function n(
+	/**
+	 * Number of empty lines to log.
+	 * @defaultValue 1
+	 */
+	count = 1,
+) {
+	for (let i = 0; i < count; i++) console.log()
+}
+
+n()
+
+/** JSON.Stringify */
+export function j(o: unknown) {
+	return stringify(o, 2)
+}
+
+/** Short timestamp. */
+export function timestamp() {
+	return new Date()
+		.toISOString()
+		.replace(/[-:.TZ]/g, '')
+		.slice(2, 12)
+}
