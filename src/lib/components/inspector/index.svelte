1629697264bccdf927f745b02bce380a57f1d680
Braden Wiggins <braden@fractal-hq.com>
2023-10-06 19:04:54 -0400

    [fix] update inspector stores

--- src/lib/components/inspector/index.svelte
+++ src/lib/components/inspector/index.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-	import { activeBookmarks, activeFolder, activeFolderBookmarks, tagFilter } from '$lib/data/dbStore'
-	import { gridDimensions, grid } from '$lib/stores/gridStore'
-	import { settings, showSettings } from '$lib/stores'
-
 	import Inspector from './Inspector.svelte'
-	import * as stores from '$lib/stores'
+	import {
+		mobileThreshold,
+		screenW,
+		screenH,
+		scrollY,
+		mobile,
+		mouse,
+	} from '../../stores/Device.svelte'
 </script>
 
 <Inspector
 	register={{
-		...stores,
-		activeBookmarks,
-		activeFolder,
-		activeFolderBookmarks,
-		tagFilter,
-		settings,
-		showSettings,
-		gridDimensions,
-		grid,
+		mobile,
+		mobileThreshold,
+		mouse,
+		screenW,
+		screenH,
+		scrollY,
 	}}
 />
