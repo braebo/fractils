25907de320741a15b2561c9ffdc3938f2d45b406
Braden Wiggins <braden@fractal-hq.com>
2023-10-08 20:31:55 -0400

    [chore] formatting

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,55 +4,54 @@
 
 // Stores
 
-export { mobile, screenH, screenW, scrollY, mouse, mobileThreshold } from './stores/Device.svelte';
-export { default as Fractils } from './stores/Device.svelte'; // todo - deprecate legacy name
-export { default as Device } from './stores/Device.svelte';
+export { mobile, screenH, screenW, scrollY, mouse, mobileThreshold } from './stores/Device.svelte'
+export { default as Fractils } from './stores/Device.svelte' // todo - deprecate legacy name
+export { default as Device } from './stores/Device.svelte'
 
 // Components
 
-export { default as Inspector } from './components/inspector/Inspector.svelte'; // todo
-export { default as MacScrollbar } from './components/MacScrollbar.svelte';
-export { default as ThemeToggle } from './components/ThemeToggle.svelte';
-export { Toasts, toast, type Toast } from './components/toast/index'; // todo
-export { default as OnMount } from './components/OnMount.svelte';
-export { default as Tooltip } from './components/Tooltip.svelte'; // todo
-export { default as Switch } from './components/Switch.svelte'; // todo
-export { default as Github } from './components/Github.svelte'; // todo
-export { default as Range } from './components/Range.svelte'; // todo
-export { default as Code } from './components/Code.svelte'; // todo
+export { default as Inspector } from './components/inspector/Inspector.svelte' // todo
+export { default as MacScrollbar } from './components/MacScrollbar.svelte'
+export { default as ThemeToggle } from './components/ThemeToggle.svelte'
+export { Toasts, toast, type Toast } from './components/toast/index' // todo
+export { default as OnMount } from './components/OnMount.svelte'
+export { default as Tooltip } from './components/Tooltip.svelte' // todo
+export { default as Switch } from './components/Switch.svelte' // todo
+export { default as Github } from './components/Github.svelte' // todo
+export { default as Range } from './components/Range.svelte' // todo
+export { default as Code } from './components/Code.svelte' // todo
 
 // Theme
 
-export { initTheme, toggleTheme, applyTheme, theme } from './theme/theme';
+export { initTheme, toggleTheme, applyTheme, theme } from './theme/theme'
 
 // Utils
 
-export { asyncLocalStorageStore } from './utils/asyncLocalStorageStore';
-export { localStorageStore } from './utils/localStorageStore';
-export { stringify, serialize } from './utils/stringify'; // todo
-export { entries, keys, values } from './utils/object'; // todo
-export { decimalToPow } from './utils/decimalToPow'; // todo
-export { colors, randomColor } from './utils/color'; // todo
-export { Extractor } from './utils/Extractor'; // todo
-export { truncate } from './utils/truncate'; // todo
-export { mapRange } from './utils/mapRange';
-export { defer } from './utils/defer'; // todo
-export { clamp } from './utils/clamp';
-export { wait } from './utils/wait';
-export { log } from './utils/log';
+export { asyncLocalStorageStore } from './utils/asyncLocalStorageStore'
+export { localStorageStore } from './utils/localStorageStore'
+export { stringify, serialize } from './utils/stringify' // todo
+export { entries, keys, values } from './utils/object' // todo
+export { decimalToPow } from './utils/decimalToPow' // todo
+export { colors, randomColor } from './utils/color' // todo
+export { truncate } from './utils/truncate' // todo
+export { mapRange } from './utils/mapRange'
+export { defer } from './utils/defer' // todo
+export { clamp } from './utils/clamp'
+export { wait } from './utils/wait'
+export { log } from './utils/log'
 
 // Actions
 
-export { add, type AddOptions } from './actions/add'; // todo
+export { add, type AddOptions } from './actions/add' // todo
 
-export { clickOutside } from './actions/clickOutside';
+export { clickOutside } from './actions/clickOutside'
 export type {
 	ClickOutsideEventDetail,
 	ClickOutsideOptions,
 	ClickOutsideEvent,
-} from './actions/clickOutside';
+} from './actions/clickOutside'
 
-export { visibility } from './actions/visibility';
+export { visibility } from './actions/visibility'
 export type {
 	VisibilityEventDetail,
 	VisibilityOptions,
@@ -62,13 +61,13 @@ export type {
 	Direction,
 	Position,
 	Event,
-} from './actions/visibility';
+} from './actions/visibility'
 
-export { highlight } from './actions/highlight'; // todo
+export { highlight } from './actions/highlight' // todo
 export type {
 	HighlightEventDetail,
 	HighlightOptions,
 	HighlightEvent,
 	HighlightAttr,
 	ValidLanguage,
-} from './actions/highlight'; // todo
+} from './actions/highlight' // todo
