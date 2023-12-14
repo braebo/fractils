b1374accab2b9a59035c3b09d6d0ad6c59ca6b4f
Braden Wiggins <braden@fractal-hq.com>
2023-12-13 19:42:52 -0500

    [feat] separate header comp

---
+++ src/examples/_lib/Header.svelte
@@ -0,0 +1,31 @@
+<template lang="pug">
+
+	header
+		h1 fractils
+		p.description
+			span Some stuff I often use...
+
+</template>
+
+<style>
+	header {
+		padding-top: 0.5rem;
+
+		color: white;
+	}
+
+	h1,
+	p {
+		padding: 0 1rem;
+
+		color: white;
+	}
+
+	span {
+		font-variation-settings: 'wght' 200;
+		font-family: var(--font-b);
+		font-size: 0.9rem;
+
+		opacity: 0.5;
+	}
+</style>
---
+++ src/examples/_lib/InstallCommand.svelte
@@ -0,0 +1,20 @@
+<h2> pnpm i -D fractils </h2>
+
+<style>
+	h2 {
+		margin: 1.5rem auto 0 auto;
+		padding: 6px 10px 7px 10px;
+		width: max-content;
+
+		font-family: var(--font-mono);
+		letter-spacing: 1px;
+		text-align: center;
+		font-size: 1.1rem;
+		font-weight: 300;
+
+		color: var(--brand-a);
+
+		border-radius: var(--border-radius);
+		background-color: rgb(21, 22, 29);
+	}
+</style>
