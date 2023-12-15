598a90026b897e2370b68cba869a694ddee264f3
Braden Wiggins <braden@fractal-hq.com>
2023-12-15 17:18:48 -0500

    [feat] change `isSafari` into a function

--- src/lib/utils/safari.ts
+++ src/lib/utils/safari.ts
@@ -1,8 +1,12 @@
 /**
  * Checks if the current browser is a potato (Safari).
  */
-export const isSafari =
-	typeof navigator !== 'undefined' && /^((?!chrome|android).)*safari/i.test(navigator?.userAgent);
+export function isSafari() {
+	return (
+		typeof globalThis.navigator !== 'undefined' &&
+		/^((?!chrome|android).)*safari/i.test(navigator?.userAgent)
+	)
+}
 
 /**
  * Helper function to deal with the flaming hot pile of garbage that is Safari.
@@ -10,5 +14,5 @@ export const isSafari =
  * @returns A less cool (safari compatible) thing if safari is detected, otherwise the cool thing.
  */
 export function maybeCoolThing<CoolThing, LessCoolThing>(a: CoolThing, b: LessCoolThing) {
-	return isSafari ? b : a;
+	return isSafari() ? b : a
 }
