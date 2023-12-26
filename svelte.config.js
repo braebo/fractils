169b172402b410bcc2c9e534ac083c53ac6f25a2
Braden Wiggins <braden@fractal-hq.com>
2023-12-26 18:27:43 -0500

    [wip] inline highlighter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,27 @@
+// import { mdsvex, escapeSvelte } from 'mdsvex'
 import vercel from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
 import { resolve } from 'path'
+// import shiki from 'shiki'
+
+// /** @type {import('mdsvex').MdsvexOptions} */
+// const mdsvexOptions = {
+// 	extensions: ['.md', '.svelte'],
+// 	highlight: {
+// 		highlighter: async (code, lang = 'text') => {
+// 			const highlighter = await shiki.getHighlighter({ theme: 'poimandres' })
+// 			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
+// 			return `{@html \`${html}\` }`
+// 		},
+// 	},
+// }
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [preprocess()],
+	preprocess: [
+		// mdsvex(mdsvexOptions),
+		preprocess(),
+	],
 	kit: {
 		adapter: vercel(),
 		alias: {
@@ -12,10 +29,17 @@ const config = {
 			$examples: resolve('./src/examples'),
 			$scripts: resolve('./src/scripts'),
 		},
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				if (path === '/code/ssr') {
+					return
+				}
+			},
+		},
 	},
 	vitePlugin: {
 		inspector: {
-			toggleKeyCombo: 'control-meta'
+			toggleKeyCombo: 'control-meta',
 		},
 	},
 }
