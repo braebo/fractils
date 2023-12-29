b1f0c0c67615f1f19c5a2821836580acad5da1bd
Braden Wiggins <braden@fractal-hq.com>
2023-12-29 09:00:20 -0500

    [wip] test pipeline

---
+++ src/lib/test/Test.svelte
@@ -0,0 +1,45 @@
+<!-- 
+    @component
+
+    This is a [multiline](https://example.com)
+    line comment.
+
+    With a second {@link paragraph}.
+
+    And two examples, the first unnamed, and the second named with a filename/title.
+
+    @example
+
+    ```typescript
+    console.log('hello world')
+    ```
+
+    @example Svelte Example
+
+    ```svelte Example.svelte
+    <script>
+        console.log('hello world')
+    </script>
+    ```
+
+    And using this comp:
+
+    ```svelte +page.svelte
+    <Test text="duder" />
+    ```
+ -->
+
+<script lang="ts">
+	/**
+	 * Super duper important text.
+	 */
+	export let text = 'world'
+	/**
+	 * Super duper important text.
+	 */
+	export let mynan = 'based'
+
+    console.log(mynan)
+</script>
+
+<h1>Hello {text}</h1>
