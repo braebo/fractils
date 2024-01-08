1b1be7c679475d58642ea84e8e355ad0d2889537
Braden Wiggins <braden@fractal-hq.com>
2024-01-07 23:19:18 -0500

    [feat] nanoid

---
+++ src/lib/utils/nanoid.ts
@@ -0,0 +1,16 @@
+export function nanoid(t = 21) {
+	return crypto
+		.getRandomValues(new Uint8Array(t))
+		.reduce(
+			(t, e) =>
+				(t +=
+					(e &= 63) < 36
+						? e.toString(36)
+						: e < 62
+							? (e - 26).toString(36).toUpperCase()
+							: e > 62
+								? '-'
+								: '_'),
+			'',
+		)
+}
