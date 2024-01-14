a6bb5d84e387c4c6c94cdf4add36dae54d53feee
Braden Wiggins <braden@fractal-hq.com>
2024-01-14 15:12:38 -0500

    [feat] remove scss from error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script>
 	import FourOhFour from '../examples/_lib/FourOhFour.svelte'
 	import { page } from '$app/stores'
 	import { DEV } from 'esm-env'
@@ -19,12 +19,12 @@
 
 <slot />
 
-<style lang="scss">
+<style>
 	a {
 		display: flex;
 
 		width: max-content;
-		// margin: -5rem auto 0 auto;
+		/* margin: -5rem auto 0 auto; */
 
 		font-size: 1.5rem;
 		text-align: center;
@@ -42,40 +42,40 @@
 
 		margin-top: 10vh;
 		text-align: center;
+	}
 
-		pre {
-			max-width: 90vw;
+	.error::-webkit-scrollbar {
+		background-color: var(--light-a);
+		width: 10px;
+		height: 10px;
+	}
+	.error::-webkit-scrollbar-thumb {
+		background-color: rgba(var(--light-d-rgb), 0.5);
+		border-radius: 5px;
+	}
+	.error::-webkit-scrollbar-track {
+		background-color: rgba(var(--light-d-rgb), 0.1);
+	}
+	.error::-webkit-scrollbar-corner {
+		background-color: rgba(var(--light-d-rgb), 0.1);
+	}
 
-			text-align: left;
-			line-height: 1.5rem;
-		}
+	pre {
+		max-width: 90vw;
 
-		.message {
-			width: max-content;
-			height: max-content;
-			margin: 1rem auto;
-			padding: 1rem;
+		text-align: left;
+		line-height: 1.5rem;
+	}
 
-			color: var(--dark-d);
-			background: transparent;
-			border: 1px solid var(--light-d);
-			border-radius: var(--radius-lg);
-		}
+	.message {
+		width: max-content;
+		height: max-content;
+		margin: 1rem auto;
+		padding: 1rem;
 
-		::-webkit-scrollbar {
-			background-color: var(--light-a);
-			width: 10px;
-			height: 10px;
-		}
-		::-webkit-scrollbar-thumb {
-			background-color: rgba(var(--light-d-rgb), 0.5);
-			border-radius: 5px;
-		}
-		::-webkit-scrollbar-track {
-			background-color: rgba(var(--light-d-rgb), 0.1);
-		}
-		::-webkit-scrollbar-corner {
-			background-color: rgba(var(--light-d-rgb), 0.1);
-		}
+		color: var(--dark-d);
+		background: transparent;
+		border: 1px solid var(--light-d);
+		border-radius: var(--radius-lg);
 	}
 </style>
