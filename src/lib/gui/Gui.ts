1ba1fb68ac1e83c13aeaab393c3d495811755225
Braden Wiggins <braden@fractal-hq.com>
2024-01-17 22:24:02 -0500

    [feat] improved / isolated `Gui`

--- src/lib/gui/Gui.ts
+++ src/lib/gui/Gui.ts
@@ -1,365 +1,72 @@
 import type { Resizable, ResizableOptions } from '../utils/resizable'
 import type { Draggable, DragOptions } from '../utils/draggable'
 import type { ThemerOptions } from '../theme/Themer'
+import type { FolderOptions } from './Folder'
 
-import { state, type State } from '../utils/state'
 import { entries } from '../utils/object'
-import { create } from '../utils/create'
 import { logger } from '../utils/logger'
-import { Themer } from '../theme/Themer'
-import { nanoid } from '../utils/nanoid'
+import { state, type PrimitiveState, type State } from '../utils/state'
 import { fn, g, r } from '../utils/l'
+
+import { Themer } from '../theme/Themer'
+import { Folder } from './Folder'
+
 import { BROWSER } from 'esm-env'
 import './gui.scss'
 
-type InputType =
-	| 'Text'
-	| 'Number'
-	| 'Boolean'
-	| 'Color'
-	| 'Range'
-	| 'Select'
-	| 'Button'
-	| 'Folder'
-	| 'Textarea'
-
-type InputValue<T = InputType> = T extends 'Text'
-	? string
-	: T extends 'Number'
-		? number
-		: T extends 'Boolean'
-			? boolean
-			: T extends 'Color'
-				? string
-				: T extends 'Range'
-					? number
-					: T extends 'Select'
-						? string
-						: T extends 'Button'
-							? void
-							: T extends 'Folder'
-								? Folder
-								: T extends 'Textarea'
-									? string
-									: never
-
-export interface InputOptions<T = InputType, V = InputValue<T>> {
-	value: V
-	title: string
-	type: string
-	folder: Folder
-}
-
-export class Input<T = InputType, V = InputValue<T>> {
-	state: State<V>
-	title: string
-	type: string
-	folder: Folder
-	element: HTMLElement
-
-	constructor(options: InputOptions<T, V>) {
-		this.state = state(options.value)
-		this.title = options.title
-		this.type = options.type
-		this.folder = options.folder
-		this.element = this.#createElement()
-	}
-
-	#createElement() {
-		const element = create('div', {
-			classes: ['gui-control'],
-			parent: this.folder.contentElement,
-		})
-
-		create('label', {
-			classes: ['gui-label'],
-			parent: element,
-			textContent: this.title,
-		})
-
-		return element
-	}
-}
-
-/**
- * @internal
- */
-export interface FolderOptions {
-	/**
-	 * The title of the folder.
-	 * @default ''
-	 */
-	title: string
-	/**
-	 * The child folders of this folder.
-	 */
-	children: Folder[]
-	/**
-	 * Any controls this folder should contain.
-	 */
-	controls: Map<string, Input>
-	parentFolder: Folder
-	/**
-	 * Whether the folder should be collapsed by default.
-	 * @default false
-	 */
-	closed: boolean
-	/**
-	 * The element to append the folder to (usually
-	 * the parent folder's content element).
-	 */
-	element?: HTMLElement
-}
-
-/**
- * @internal
- */
-export class Folder {
-	id = nanoid()
-	isFolder = true as const
-
-	isRoot = false
-	root: Folder
-
-	title: string
-	children: Folder[]
-	controls: Map<string, Input>
-	parentFolder: Folder
-
-	element: HTMLElement
-	headerElement!: HTMLElement
-
-	titleElement!: HTMLElement
-	contentElement!: HTMLElement
-
-	closed = state(false)
-
-	log = logger('Folder', {
-		fg: 'DarkSalmon',
-		deferred: false,
-		server: false,
-	})
-
-	constructor(options: FolderOptions, rootContainer: HTMLElement | null = null) {
-		const opts = Object.assign({}, options)
-		this.log(g('constructor') + '()', { opts, this: this })
-
-		this.title = opts.title ?? ''
-		this.children = opts.children ?? []
-		this.controls = opts.controls ?? new Map<string, Input>()
-
-		if (rootContainer) {
-			this.root = this
-			this.isRoot = true
-			this.parentFolder = this
-			const rootEl = this.#createRootElement(rootContainer)
-			this.element = this.#createElements(rootEl)
-		} else {
-			this.parentFolder = opts.parentFolder
-			this.root = this.parentFolder.root
-			this.element = this.#createElements(opts.element)
-		}
-
-		if (opts.closed) this.closed.set(opts.closed)
-
-		// Open/close the folder when the closed state changes.
-		this.#subs.push(this.closed.subscribe((v) => (v ? this.close() : this.open())))
-	}
-
-	#subs: Array<() => void> = []
-
-	/**
-	 * Used to disable clicking the header to open/close the folder.
-	 */
-	#disabledTimer?: NodeJS.Timeout
-	/**
-	 * The time in ms to wait after mousedown before
-	 * disabling toggle for a potential drag.
-	 */
-	#clickTime = 200
-	/**
-	 * Whether clicking the header to open/close the folder is disabled.
-	 */
-	#disabled = false
-
-	#skip_header_click_if_drag = (event: PointerEvent) => {
-		if (event.button !== 0) return
-
-		addEventListener('pointerup', this.toggle, { once: true })
-
-		// We need to watch for the mouseup event within a certain timeframe
-		// to make sure we don't accidentally trigger a click after dragging.
-		clearTimeout(this.#disabledTimer)
-		// First we delay the drag check to allow for messy clicks.
-		this.#disabledTimer = setTimeout(() => {
-			this.headerElement.addEventListener('pointermove', this.disable, { once: true })
-
-			// Then we set a timer to disable the drag check.
-			this.#disabledTimer = setTimeout(() => {
-				this.headerElement.removeEventListener('pointermove', this.disable)
-				this.#disabled = false
-			}, this.#clickTime)
-		}, 150)
-
-		if (this.#disabled) return
-	}
-
-	disable = () => {
-		if (!this.#disabled) {
-			this.#disabled = true
-			this.log(fn('disable'), 'Clicks DISABLED')
-		}
-		this.#disabled = true
-		clearTimeout(this.#disabledTimer)
-	}
-
-	reset() {
-		this.log(fn('cancel'), ' Clicks ENABLED')
-		removeEventListener('pointerup', this.toggle)
-		this.#disabled = false
-	}
-
-	#createElements(el?: HTMLElement) {
-		const element =
-			el ??
-			create('div', {
-				parent: this.parentFolder.contentElement,
-				classes: ['gui-folder'],
-				dataset: { id: this.id },
-			})
-
-		this.headerElement = create('div', {
-			parent: element,
-			classes: ['gui-header'],
-		})
-		this.headerElement.addEventListener('pointerdown', this.#skip_header_click_if_drag)
-
-		this.titleElement = create('h2', {
-			parent: this.headerElement,
-			classes: ['gui-title'],
-			textContent: this.title,
-		})
-
-		this.contentElement = create('div', {
-			classes: ['gui-content'],
-			parent: element,
-		})
-
-		return element
-	}
-
-	#createRootElement(container: HTMLElement | null = null) {
-		container ??= document.body
-
-		const rootEl = create('div', {
-			classes: ['gui-root'],
-			id: 'gui-root',
-			dataset: { id: this.id, title: this.title },
-		})
-
-		return rootEl
-	}
-
-	addFolder(options?: { title?: string; closed?: boolean }) {
-		const folder = new Folder({
-			title: options?.title ?? '',
-			controls: new Map(),
-			parentFolder: this,
-			children: [],
-			closed: options?.closed ?? false,
-		})
-
-		this.children.push(folder)
-
-		return folder
-	}
-
-	addInput(control: Input) {
-		this.controls.set(control.title, control)
-		this.contentElement.appendChild(control.element)
-	}
-
-	isGui(): this is Gui {
-		return this.isRoot
-	}
-
-	toggle = () => {
-		if (this.isGui()) {
-			clearTimeout(this.#disabledTimer)
-			if (this.#disabled) {
-				this.reset()
-				return
-			}
-		}
-
-		this.closed.get() ? this.open() : this.close()
-	}
-
-	open() {
-		this.element.classList.remove('closed')
-		this.closed.set(false)
-		this.#disabled = false
-	}
-
-	close() {
-		this.element.classList.add('closed')
-		this.closed.set(true)
-		this.#disabled = false
-	}
-
-	/**
-	 * A flat array of all children of this folder.
-	 */
-	get allChildren() {
-		return this.children.flatMap((child) => [child, ...child.allChildren])
-	}
-
-	dispose() {
-		this.#subs.forEach((unsub) => unsub())
-
-		this.headerElement.removeEventListener('click', this.toggle)
-		this.headerElement.addEventListener('pointerdown', this.#skip_header_click_if_drag)
-
-		this.element.remove()
-
-		try {
-			this.parentFolder.children.splice(this.parentFolder.children.indexOf(this), 1)
-		} catch (err) {
-			this.log(r('Error removing folder from parent'), { err })
-		}
-	}
-}
-
-/**
- * Each key provided will result in a state property being persisted
- * to localStorage under the specified key.  When persisting, the
- * state will be loaded from localStorage on initialization, and
- * saved to localStorage on update.
- */
-interface LocalStorageKeys {
-	/**
-	 * Specify to load and save the gui's position to localStorage.
-	 * @default 'fractils::gui::position'
-	 */
-	position?: string
-	/**
-	 * Specify to load and save the gui's size to localStorage.
-	 * @default 'fractils::gui::size'
-	 */
-	size?: string
-	/**
-	 * Specify to load and save the gui's closed state to localStorage.
-	 * @default 'fractils::gui::closed'
-	 */
-	closed?: string
-}
+// /**
+//  * Each key provided will result in a state property being persisted
+//  * to localStorage under the specified key.  When persisting, the
+//  * state will be loaded from localStorage on initialization, and
+//  * saved to localStorage on update.
+//  */
+// interface LocalStorageKeys {
+// 	/**
+// 	 * Specify to load and save the gui's position to localStorage.
+// 	 * @default 'fractils::gui::position'
+// 	 */
+// 	position?: string
+// 	/**
+// 	 * Specify to load and save the gui's size to localStorage.
+// 	 * @default 'fractils::gui::size'
+// 	 */
+// 	size?: string
+// 	/**
+// 	 * Specify to load and save the gui's closed state to localStorage.
+// 	 * @default 'fractils::gui::closed'
+// 	 */
+// 	closed?: string
+// }
 
 export interface GuiOptions extends FolderOptions {
+	// /**
+	//  * Determines what to persist in localStorage, and under what keys.
+	//  * @default undefined
+	//  */
+	// localStorageKeys?: LocalStorageKeys
 	/**
-	 * Determines what to persist in localStorage, and under what keys.
+	 * Persist the gui's state to localStorage by specifying the key
+	 * to save the state under.
 	 * @default undefined
 	 */
-	localStorageKeys?: LocalStorageKeys
+	persist?: {
+		/**
+		 * @default "fractils::gui"
+		 */
+		key: string
+		/**
+		 * @default true
+		 */
+		size?: boolean
+		/**
+		 * @default true
+		 */
+		position?: boolean
+		/**
+		 * @default true
+		 */
+		closed?: boolean
+	}
 	/**
 	 * The container to append the gui to.
 	 * @default document.body
@@ -396,14 +103,17 @@ export const GUI_DEFAULTS: Omit<GuiOptions, 'parentFolder'> = {
 	children: [],
 	themer: true,
 	themerOptions: {},
-	localStorageKeys: undefined,
 	resizable: true,
 	draggable: true,
 	closed: false,
+	persist: undefined,
+	// localStorageKeys: undefined,
 }
 
 /**
- * The Gui gui class
+ * The root Gui instance.  This is the entry point for creating
+ * a gui.  You can create multiple root guis, but each gui
+ * can only have one root.
  */
 export class Gui extends Folder {
 	isRoot = true as const
@@ -414,66 +124,66 @@ export class Gui extends Folder {
 	resizable?: Resizable
 	draggable?: Draggable
 
+	// /**
+	//  * Which state properties to persist to localStorage,
+	//  * and under what keys.
+	//  */
+	// persist = {
+	// 	position: 'fractils::gui::position',
+	// 	size: 'fractils::gui::size',
+	// 	closed: 'fractils::gui::closed',
+	// 	// todo - [{ key: 'Foo Folder', open: true }, ... }] ?
+	// 	// children: 'fractils::gui::children',
+	// }
+
 	/**
-	 * Which state properties to persist to localStorage,
-	 * and under what keys.
+	 * Which state properties to persist to localStorage.
 	 */
 	persist = {
-		position: 'fractils::gui::position',
-		size: 'fractils::gui::size',
-		closed: 'fractils::gui::closed',
+		key: 'fractils::gui',
+		position: false,
+		size: false,
+		closed: false,
+		// todo - [{ key: 'Foo Folder', open: true }, ... }] ?
+		// children: 'fractils::gui::children',
 	}
 
-	state = state(
-		{
-			position: { x: 0, y: 0 },
-			size: { width: 16, height: 16 },
-			closed: false,
-		},
-		{
-			set: (state) => {
-				if (this.persist.position) {
-					this.save('fractils::gui::position', state.position)
-				}
-				if (this.persist.size) {
-					this.save('fractils::gui::size', state.size)
-				}
-				if (this.persist.closed) {
-					this.save('fractils::gui::closed', state.closed)
-				}
-				return state
-			},
-		},
-	)
+	state: PrimitiveState<{
+		position: { x: number; y: number }
+		size: { width: number; height: number }
+		closed: boolean
+	}>
 
-	/**
-	 * Safely saves a single state property to localStorage.
-	 * @param key - The key to save the state under.
-	 * @param data - The data to save.
-	 */
-	save(key: string, data: any) {
-		if (typeof window === 'undefined') return
-		if (typeof localStorage === 'undefined') return
+	// /**
+	//  * Safely saves a single state property to localStorage.
+	//  * @param key - The key to save the state under.
+	//  * @param data - The data to save.
+	//  */
+	// save(key: string, data: any) {
+	// 	if (typeof window === 'undefined') return
+	// 	if (typeof localStorage === 'undefined') return
 
-		if (typeof data !== 'string') data = JSON.stringify(data)
-		localStorage.setItem(key, data)
-		this.log(fn('save'), { key, data })
-	}
+	// 	if (typeof data !== 'string') data = JSON.stringify(data)
 
-	/**
-	 * Loads the gui's state from localStorage.
-	 */
-	load(config = this.persist) {
-		this.log(fn('load'), { config })
-		for (const [key, persist] of entries(config)) {
-			if (persist) {
-				const state = localStorage.getItem(`fractils::gui::${key}`)
-				if (state) {
-					this.state.update((v) => ({ ...v, [key]: JSON.parse(state) }))
-				}
-			}
-		}
-	}
+	// 	localStorage.setItem(key, data)
+
+	// 	this.log(fn('save'), { key, data })
+	// }
+
+	// /**
+	//  * Loads the gui's state from localStorage.
+	//  */
+	// load(config = this.persist) {
+	// 	this.log(fn('load'), { config })
+	// 	for (const [key, persist] of entries(config)) {
+	// 		if (persist) {
+	// 			const state = localStorage.getItem(`fractils::gui::${key}`)
+	// 			if (state) {
+	// 				this.state.update((v) => ({ ...v, [key]: JSON.parse(state) }))
+	// 			}
+	// 		}
+	// 	}
+	// }
 
 	log = logger('Gui', {
 		fg: 'PaleVioletRed',
@@ -494,6 +204,28 @@ export class Gui extends Folder {
 
 		this.root = this
 		this.container = opts.container
+		this.state = state(
+			{
+				position: { x: 0, y: 0 },
+				size: { width: 16, height: 16 },
+				// todo - Booleans are messed up...
+				// closed: false, //! TypeError: Type 'boolean' is not assignable to type 'false'
+				// closed: false as boolean, // 👀
+				closed: opts.closed,
+			},
+			{
+				key: 'fractils::gui::state',
+				debounce: 50,
+			},
+		)
+		this.closed = state(opts.closed, {
+			onChange: (v) => {
+				this.log(fn('state.onChange'), v)
+				if (this.persist.closed) {
+					this.state.update((vv) => ({ ...vv, closed: v }))
+				}
+			},
+		})
 
 		if (opts.themer) {
 			if (opts.themer === true) {
@@ -503,7 +235,8 @@ export class Gui extends Folder {
 			}
 		}
 
-		this.#setupPersistence(opts)
+		Object.assign(this.persist, opts.persist ?? {})
+		// this.#setupPersistence(opts)
 
 		if (opts.resizable) {
 			const resizeOptions = typeof opts.resizable === 'object' ? opts.resizable : {}
@@ -512,32 +245,32 @@ export class Gui extends Folder {
 				const opts: ResizableOptions = resizeOptions
 
 				if (this.persist.size) {
-					let debounce: NodeJS.Timeout
+					// let debounce: NodeJS.Timeout
 
 					opts.onResize = (size) => {
-						if (!size) {
-							this.log(fn('onResize'), r('Error: No size to save'))
-							return
-						}
+						this.state.update((v) => ({ ...v, size }))
+						// if (!size) {
+						// 	this.log(fn('onResize'), r('Error: No size to save'))
+						// 	return
+						// }
 
-						clearTimeout(debounce)
+						// clearTimeout(debounce)
 
-						debounce = setTimeout(() => {
-							this.state.update((v) => ({ ...v, size }))
+						// debounce = setTimeout(() => {
+						// this.state.update((v) => ({ ...v, size }))
 
-							this.log(fn('resizable.onResize'), 'Saving size.', size)
-							this.save('fractils::gui::size', size)
-						}, 100)
+						// this.log(fn('resizable.onResize'), 'Saving size.', size)
+						// this.save('fractils::gui::size', size)
+						// }, 100)
 					}
 				}
 
 				this.resizable = new Resizable(this.element, opts)
 
+				// Load size from state.
 				if (this.persist.size) {
 					const state = this.state.get()
-
 					this.log(fn('constructor'), 'Loading size from state.', state.size)
-
 					if (opts?.sides?.includes('left') || opts?.sides?.includes('right')) {
 						this.element.style.width = `${state.size.width}px`
 					}
@@ -558,6 +291,7 @@ export class Gui extends Folder {
 				drag: true,
 			}
 
+			//· todo - move this into the draggable class ··································¬
 			// This makes sure the gui is in the viewport.
 			const offscreenCheck = () => {
 				const state = this.state.get()
@@ -603,32 +337,44 @@ export class Gui extends Folder {
 			}
 
 			dragOptions.position = offscreenCheck()
+			//⌟
 
-			let debounce: NodeJS.Timeout
+			// let debounce: NodeJS.Timeout
 			import('../utils/draggable').then(({ Draggable }) => {
 				this.draggable = new Draggable(this.element, {
 					...dragOptions,
 					onDragEnd: this.persist.position
 						? (data) => {
-								const pos = { x: data.offsetX, y: data.offsetY }
-								if (!pos) {
-									this.log(r('onDragEnd'), 'No position to save')
-									return
-								}
+								this.state.update((v) => ({
+									...v,
+									position: { x: data.offsetX, y: data.offsetY },
+								}))
+								// const pos = { x: data.offsetX, y: data.offsetY }
+								// if (!pos) {
+								// 	this.log(r('onDragEnd'), 'No position to save')
+								// 	return
+								// }
 
-								this.log(fn('onDragEnd'), 'Saving position', pos)
+								// this.log(fn('onDragEnd'), 'Saving position', pos)
 
-								clearTimeout(debounce)
-								debounce = setTimeout(() => {
-									this.state.update((v) => ({ ...v, position: pos }))
+								// clearTimeout(debounce)
+								// debounce = setTimeout(() => {
+								// this.state.update((v) => ({ ...v, position: pos }))
 
-									this.log(fn('gui.draggable.onDragEnd'), 'Saving position.', pos)
-									this.save('fractils::gui::position', pos)
-								}, 100)
+								// this.log(fn('gui.draggable.onDragEnd'), 'Saving position.', pos)
+								// 	this.save('fractils::gui::position', pos)
+								// }, 100)
 							}
 						: undefined,
 				})
 			})
+		}
+
+		// Load closed from state.
+		if (this.persist.closed) {
+			const { closed } = this.state.get()
+			this.log(fn('constructor'), 'Loading closed from state.', closed)
+			closed ? this.open() : this.close()
 		}
 
 		setTimeout(() => {
@@ -644,17 +390,16 @@ export class Gui extends Folder {
 		return this
 	}
 
-	#setupPersistence(opts: GuiOptions) {
-		if (opts.localStorageKeys !== undefined) {
-			this.persist = {
-				position: opts.localStorageKeys?.position ?? '',
-				size: opts.localStorageKeys?.size ?? '',
-				closed: opts.localStorageKeys?.closed ?? '',
-			}
-		}
-
-		this.load()
-	}
+	// #setupPersistence(opts: GuiOptions) {
+	// 	if (opts.localStorageKeys !== undefined) {
+	// 		this.persist = {
+	// 			position: opts.localStorageKeys?.position ?? '',
+	// 			size: opts.localStorageKeys?.size ?? '',
+	// 			closed: opts.localStorageKeys?.closed ?? '',
+	// 		}
+	// 	}
+	// 	this.load()
+	// }
 
 	dispose() {
 		super.dispose()
