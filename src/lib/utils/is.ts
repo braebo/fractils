b9c0bf52c319920ea1f82c9bd87b60efe7123a7d
Braden Wiggins <braden@fractal-hq.com>
2024-01-17 22:25:47 -0500

    [feat] `isString` and `isDefined`

--- src/lib/utils/is.ts
+++ src/lib/utils/is.ts
@@ -5,3 +5,11 @@ export function isTouchEvent(e: Event): e is TouchEvent {
 export function isMouseEvent(e: Event): e is PointerEvent {
 	return e.type.startsWith('mouse')
 }
+
+export function isDefined<T>(value: T | undefined): value is T {
+	return value !== undefined
+}
+
+export function isString(value: unknown): value is string {
+	return typeof value === 'string'
+}
