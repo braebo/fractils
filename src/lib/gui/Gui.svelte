2eaa4e85dde3d5aaa088428ea50b5f299e3bfa4b
Braden Wiggins <braden@fractal-hq.com>
2024-01-24 16:38:21 -0500

    [fix] use new presistence api

--- src/lib/gui/Gui.svelte
+++ src/lib/gui/Gui.svelte
@@ -13,7 +13,10 @@
 		if (!gui) {
 			gui = new module.Gui({
 				element: options.container || containerEl,
-				persistence: options.persistence ?? true,
+				// localStorageKeys: options.localStorageKeys ?? true,
+				storage: options.storage ?? {
+					key: 'fractils::gui-svelte',
+				},
 			})
 		}
 	})
@@ -31,22 +34,5 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-
-		// min-width: 25vw;
-		// min-height: 25vw;
-
-		// width: var(--width, 30rem);
-		// height: var(--height, 30rem);
-
-		// background: rgba(var(--bg-a-rgb), 0.8);
-		// outline: 1px solid rgba(var(--bg-b-rgb), 0.5);
-		// border-radius: var(--radius);
-		// backdrop-filter: blur(0.2rem);
-		// box-shadow:
-		// 	var(--shadow-lg),
-		// 	inset 0 0 10px rgba(var(--bg-b-rgb), 0.33);
-
-		// z-index: 9999;
-		// overflow: visible;
 	}
 </style>
