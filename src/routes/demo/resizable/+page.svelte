dfcd9471f03e42bf46ea599e5b9d4a9227dc8784
Braden Wiggins <braden@fractal-hq.com>
2024-01-29 20:33:56 -0800

    [temp] resizable demo area

---
+++ src/routes/demo/resizable/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Orbs, { params } from './Orbs.svelte'
+	import { resizable } from '$lib'
+</script>
+
+<div class="page">
+	<div class="orbs" use:resizable={{ visible: true }}>
+		<Orbs />
+	</div>
+</div>
+
+<style lang="scss">
+	.page {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: flex-start;
+		gap: 1rem;
+
+		max-width: 400px;
+		height: 100%;
+		flex-grow: 1;
+		margin: auto;
+
+		padding: 1rem;
+	}
+
+	.orbs {
+		position: absolute;
+
+		width: 200px;
+		height: 200px;
+		margin: auto;
+	}
+</style>
---
+++ src/routes/demo/resizable/Orbs.svelte
@@ -0,0 +1,161 @@
+<script lang="ts" context="module">
+	import { tweened } from 'svelte/motion'
+	let count = 10
+	export const params = {
+		orbs: 50,
+		size: 5,
+		a1: 0.1,
+		a2: 0.5,
+		width: count * 10,
+		height: count * 10,
+		speed: 0.02,
+		mid: count * 5,
+		brightness: .4,
+	}
+	const a1t = tweened(params.a1, { duration: 500 })
+	const a2t = tweened(params.a2, { duration: 500 })
+</script>
+
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	$: time = 1
+	$: snake = circle()
+	function circle() {
+		let arr = [] as number[][]
+		for (let i = 0; i < params.orbs; i++) {
+			arr = [
+				...arr,
+				[
+					(Math.sin((i / Math.PI) * $a1t - time) / Math.PI) * params.width + params.mid,
+					(Math.cos((i / Math.PI) * $a2t - time) / Math.PI) * params.height + params.mid,
+				],
+			]
+		}
+		return arr
+	}
+
+	function animate() {
+		requestAnimationFrame(() => {
+			$a1t = params.a1
+			$a2t = params.a2
+			requestAnimationFrame(() => {
+				time += params.speed / 10
+				snake = circle()
+				animate()
+			})
+		})
+	}
+	
+	onMount(animate)
+</script>
+
+<div class="orbs">
+	<svg width="100%" height="100%">
+		<svg id="die" viewBox="0 0 100 100">
+			<g transform="rotate({0})">
+				{#each snake as [x, y], i}
+					{#if x && y}
+						{#key time}
+							<circle
+								class="dot"
+								fill="url(#orb{i})"
+								cx={x}
+								cy={y}
+								r="{Math.max(0, params.size)}px"
+							/>
+
+							<defs>
+								<radialGradient
+									id="orb{i}"
+									cx="25%"
+									cy="25%"
+									r="100%"
+									fx="35%"
+									fy="25%"
+								>
+									<stop
+										offset="0%"
+										style="stop-color:rgb({[
+											10 * (((i + 1) * params.brightness) / params.orbs + 0.5),
+											200 * (((i + 1) * params.brightness) / params.orbs + 0.5),
+											250 * (((i + 1) * params.brightness) / params.orbs + 0.5),
+										]});stop-opacity:1"
+									/>
+									<stop
+										offset="100%"
+										style="stop-color:rgb({[
+											(i + 1) * params.brightness * 10,
+											50 + (i + 1) * params.brightness * 10,
+											100 + (i + 1) * params.brightness * 50,
+										]});stop-opacity:1"
+									/>
+								</radialGradient>
+							</defs>
+						{/key}
+					{/if}
+				{/each}
+			</g>
+		</svg>
+	</svg>
+</div>
+
+<style>
+	.orbs {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		/* position: absolute; */
+		/* inset: 0; */
+		/* top: 30vh; */
+		/* margin: auto; */
+	}
+	#die {
+		width: 100%;
+		height: 100%;
+		border-radius: 5px;
+	}
+	stop {
+		transition: all 0.6s ease-in-out;
+	}
+	.dot,
+	g {
+		transition: all 0.6s ease-in-out;
+	}
+	svg {
+		backface-visibility: hidden;
+		overflow: visible;
+	}
+	/* input,
+	p {
+		height: 30px;
+		display: flex;
+		margin: auto;
+	}
+	.controls {
+		display: grid;
+		grid: auto-flow / 1fr 1fr;
+		margin: auto;
+		width: max-content;
+	} */
+	/* .wrapper {
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+		margin: auto;
+	}
+	.container {
+		display: flex;
+		flex-direction: column;
+		width: 100vw;
+	} */
+	/* :global(body, html, main) {
+		margin: 0;
+		padding: 0;
+	}
+	:global(h1) {
+		font-family: roboto;
+		text-align: center;
+		color: gray;
+	} */
+</style>
