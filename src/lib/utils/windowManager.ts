913e828f3e88e0fbf79a502b24ed1fb3aaecb8b5
dllmusic <34119160+dllmusic@users.noreply.github.com>
2024-02-14 18:07:50 -0600

    Co-authored-by: Braden Wiggins <FractalHQ@users.noreply.github.com>

--- src/lib/utils/windowManager.ts
+++ src/lib/utils/windowManager.ts
@@ -1,15 +1,134 @@
-export type WindowManagerOptions = typeof WINDOWMANAGER_DEFAULTS
+import { Draggable, type DragOptions } from './draggable3'
+import { Resizable, type ResizableOptions } from './resizable'
 
-export const WINDOWMANAGER_DEFAULTS = {}
+export interface WindowManagerOptions {
+	/**
+	 * The base z-index value.
+	 * @default 0
+	 */
+	zFloor: number
 
+	/**
+	 * Return to current z-index on release.
+	 * @default false
+	 */
+	keepZ: boolean
+
+	/**
+	 * Whether to make windows draggable.
+	 * @default true
+	 */
+	draggable: boolean | Partial<DragOptions>
+
+	/**
+	 * Whether to make windows resizable.
+	 * @default true
+	 */
+	resizable: boolean | Partial<ResizableOptions>
+}
+
+export const WINDOWMANAGER_DEFAULTS: WindowManagerOptions = {
+	zFloor: 0,
+	keepZ: false,
+	resizable: true,
+	draggable: true,
+}
+
+/**
+ * Manages the z-index of multiple elements to ensure
+ * the most recently selected element is on top.
+ */
 export class WindowManager {
 	nodes: HTMLElement[] = []
+	opts: WindowManagerOptions
 
-	constructor(options: Partial<WindowManagerOptions>) {
-		const opts = { ...WINDOWMANAGER_DEFAULTS, ...options }
+	draggableOptions: Partial<DragOptions> | null
+	resizableOptions: Partial<ResizableOptions> | null
+
+	constructor(options?: Partial<WindowManagerOptions>) {
+		this.opts = { ...WINDOWMANAGER_DEFAULTS, ...options }
+
+		console.log(this.opts.zFloor)
+
+		this.draggableOptions =
+			typeof this.opts.draggable === 'object'
+				? this.opts.draggable
+				: this.opts.draggable === true
+					? {}
+					: null
+		this.resizableOptions =
+			typeof this.opts.resizable === 'object'
+				? this.opts.resizable
+				: this.opts.resizable === true
+					? {}
+					: null
 	}
 
-	add(node: HTMLElement) {
+	add = (node: HTMLElement, options?: Partial<WindowManagerOptions>) => {
 		this.nodes.push(node)
+
+		if (this.draggableOptions) {
+			new Draggable(node, Object.assign(this.draggableOptions, options?.draggable))
+		}
+		if (this.resizableOptions) {
+			new Resizable(node, Object.assign(this.resizableOptions, options?.resizable))
+		}
+
+		node.addEventListener('pointerdown', this.select, { capture: false })
+
+		return {
+			destroy: () => {
+				node.removeEventListener('pointerdown', this.select)
+			},
+		}
+	}
+
+	applyZ() {
+		for (let i = 1; i < this.nodes.length; i++) {
+			this.nodes[i].style.setProperty('z-index', String(this.opts.zFloor + i))
+		}
+
+		return this
+	}
+
+	select = (e: PointerEvent) => {
+		const node = e.currentTarget as HTMLElement
+		const z = node.style.getPropertyValue('z-index')
+		node.style.setProperty('z-index', String(this.opts.zFloor + this.nodes.length))
+
+		// const scale = window.getComputedStyle(node).getPropertyValue('scale')
+		const scale = parseFloat(node.style.getPropertyValue('scale')) || 1
+
+		// node.style.setProperty('scale', String(scale + 0.1))
+		node.animate([{ scale }, { scale: scale + 0.025 }], {
+			duration: 75,
+			easing: 'ease-out',
+			fill: 'forwards',
+		})
+
+		const animateBack = () =>
+			node.animate({ scale: scale }, { duration: 75, easing: 'ease-in', fill: 'forwards' })
+
+		if (this.opts.keepZ) {
+			// prettier-ignore
+			addEventListener('pointerup', () => {
+				animateBack()
+				node.style.setProperty('z-index', z)
+			}, { once: true })
+		} else {
+			this.nodes = this.nodes.filter((n) => n !== node)
+			this.nodes.push(node)
+			this.applyZ()
+			// prettier-ignore
+			addEventListener('pointerup', () => {
+				animateBack()
+			}, { once: true })
+		}
+	}
+
+	dispose() {
+		for (const node of this.nodes) {
+			node.removeEventListener('pointerdown', this.select)
+		}
 	}
 }
---
+++ src/routes/demo/window-manager/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { WindowManager } from '$lib/utils/windowManager'
+
+	const windowManager = new WindowManager({
+		draggable: {
+			obstacles: '.window-4',
+		},
+		resizable: true,
+	})
+</script>
+
+{#each [1, 2, 3, 4, 5] as i}
+	<div
+		class="window-{i}"
+		style="top:{(i - 1) * 124}px"
+		use:windowManager.add={{ keepZ: i === 2 }}
+	>
+		<h1>Window {i}</h1>
+	</div>
+{/each}
+
+<style>
+	div {
+		position: absolute;
+		width: 240px;
+		height: 120px;
+		border: 1px solid red;
+
+		background-color: rgba(var(--bg-a-rgb), 0.5);
+		backdrop-filter: blur(0.25rem);
+	}
+	.window-4 {
+		color: black;
+		background-color: lightslategrey;
+	}
+</style>
