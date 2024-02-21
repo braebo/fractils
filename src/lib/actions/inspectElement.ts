a48393f10ad8b09a942c0fd304426b3bae93cc6d
Braden Wiggins <braden@fractal-hq.com>
2024-02-21 02:21:31 -0500

    [feat] lipstick on a pig

--- src/lib/actions/inspectElement.ts
+++ src/lib/actions/inspectElement.ts
@@ -1,110 +1,76 @@
 import type { Action } from 'svelte/action'
 
-// WIP
+import { create } from '$lib/utils/create'
 
 export const inspectElement: Action = (node: HTMLElement) => {
 	const elements = node.querySelectorAll('*')
-	/** popover height  */
-	const height = -20
+	const labelHeight = -20
+	const hue_offset = 30
 
 	elements.forEach((element) => {
-		;(element as HTMLElement).addEventListener('mouseenter', handleMouseEnter)
+		;(element as HTMLElement).addEventListener('pointerenter', handlePointerEnter)
 	})
 
-	let handleMouseLeave = () => void 0
+	let handlePointerLeave = () => void 0
 
-	let popovers = new Set<HTMLElement>()
+	let labels = new Set<HTMLElement>()
+	let abortFns = new Set<() => void>()
+	let largestLabel = 0
 
-	function handleMouseEnter(event: MouseEvent) {
-		const element = event.target as HTMLElement
+	let activeLabel: HTMLElement | undefined = undefined
+	let og_filter = 'none'
 
-		const popover = createPopover(element)
-		document.body.appendChild(popover)
+	let disabled = true
 
-		const outline = element.style.outline
-		const outlineOffset = element.style.outlineOffset
-
-		element.style.outline = `1px dotted hsl(${popovers.size * 30}, 100%, 50%)`
-
-		handleMouseLeave = () => {
-			popovers.delete(popover)
-			element.style.outline = outline
-			element.style.outlineOffset = outlineOffset
-			// element.style.filter = filter
-			popover.remove()
+	const toggle = (e: KeyboardEvent) => {
+		if (e.metaKey && e.key === 'Alt') {
+			disabled = false
+			window.addEventListener(
+				'keyup',
+				(e) => {
+					disabled = true
+					abortFns.forEach((fn) => fn())
+				},
+				{ once: true },
+			)
 		}
+	}
+	window.addEventListener('keydown', toggle)
 
-		// Make sure the popovers don't overlap
-		// todo - It would be better if the popovers were only moved if they overlap.
-		function organize() {
-			let i = 0
-			for (const popover of popovers) {
-				// const top = element.offsetTop
-				const top = popover.offsetTop
-				popover.style.transform = `translate(${i * 10}px, ${20 * i}px)`
-				element.style.outlineOffset = `${-4 * i}px`
-				i++
-			}
+	const updateClientXY = (e: PointerEvent) => {
+		if (disabled) return
+		for (const label of labels) {
+			label.style.transform = `translate(${e.clientX + 10}px, ${e.clientY - labels.size * labelHeight}px)`
 		}
-
-		organize()
-
-		// Remove the outline when the popover is removed
-		;(element as HTMLElement).addEventListener('mouseleave', handleMouseLeave, { once: true })
 	}
+	window.addEventListener('pointermove', updateClientXY, { passive: true })
 
-	function wrap(str: string | number, color: string) {
-		return `<span style="color: ${color}">${str}</span>`
-	}
+	function handlePointerEnter(e: PointerEvent) {
+		if (disabled) return
 
-	function createPopover(element: HTMLElement) {
+		const element = e.target as HTMLElement
+
+		const label = create('div', {
+			classes: ['inspect-element-label'],
+			parent: document.body,
+		})
+		labels.add(label)
+		setActiveLabel(label)
+
 		const tagName = element.tagName
-		const className = element.className
+		const classes = [...element.classList].join('.')
 
-		const popover = document.createElement('div')
-		popovers.add(popover)
+		const elRect = element.getBoundingClientRect()
 
-		const e = element.getBoundingClientRect()
-		const p = popover.getBoundingClientRect()
-		const vw = window.innerWidth
-		const vh = window.innerHeight
-
-		// We need to find out which side of the screen the element is closer to
-		// and position the popover on the opposite side.
-		function position() {
-			const left = e.left
-			const right = vw - e.right
-			const top = e.top
-			const bottom = vh - e.bottom
-
-			const ew = Math.min(left, right)
-
-			if (ew === left) {
-				popover.style.left = e.right + p.width + 'px'
-			}
-			if (ew === right) {
-				popover.style.right = vw - e.right + 'px'
-			}
-
-			const ns = Math.min(top, bottom)
-
-			if (ns === top) {
-				popover.style.top = e.top + 'px'
-			}
-			if (ns === bottom) {
-				popover.style.top = vh - e.bottom + 'px'
-			}
-		}
-
-		popover.classList.add('popover')
-		popover.innerHTML = `${wrap(tagName, `hsl(${popovers.size * 30}, 100%, 50%)`)}.${wrap(className, 'var(--theme-b)')}\n · ${wrap(Math.round(e.width), 'var(--theme-c)')}${wrap('x', 'gray')}${wrap(Math.round(e.height), 'var(--theme-c)')}`
+		label.classList.add('inspect-element-label')
+		label.innerHTML = `${wrap(tagName, `hsl(${(labels.size * hue_offset) % 360}, 100%, 50%)`)}.${wrap(classes, 'var(--theme-b)')} · ${wrap(Math.round(elRect.width), 'var(--theme-c)')}${wrap('x', 'gray')}${wrap(Math.round(elRect.height), 'var(--theme-c)')}`
 
 		// Position the popover atop the hovered node
-		popover.style.cssText = /*css*/ `
+		label.style.cssText = /*css*/ `
         .{
             position: absolute;
-            
-            height: ${height}px;
+
+            height: ${labelHeight}px;
             white-space: nowrap;
             padding: 3px;
             
@@ -114,19 +80,65 @@ export const inspectElement: Action = (node: HTMLElement) => {
             
             font-family: monospace;
             font-size: 0.66rem;
+
+			z-index: ${9900 + labels.size};
+			pointer-events: none;
         }
         `.replaceAll(/(\.{|})/g, '')
 
-		position()
+		//? Position the label
+		largestLabel = Math.max(largestLabel, label.offsetWidth)
 
-		return popover
+		label.style.top = labels.size * labelHeight + 'px'
+
+		const og_outline = element.style.outline
+		const og_outlineOffset = element.style.outlineOffset
+
+		element.style.outline = `1px dotted hsla(${(labels.size * hue_offset) % 360}, 100%, 50%, 0.5)`
+
+		handlePointerLeave = () => {
+			labels.delete(label)
+			element.style.outline = og_outline
+			element.style.outlineOffset = og_outlineOffset
+			label.remove()
+			largestLabel = 0
+		}
+		abortFns.add(handlePointerLeave)
+
+		let i = 0
+		for (const label of labels) {
+			i++
+			if (i === labels.size) continue
+			label.style.opacity = String((1 / labels.size) * i)
+		}
+
+		// Remove the outline when the popover is removed
+		;(element as HTMLElement).addEventListener('pointerleave', handlePointerLeave, {
+			once: true,
+		})
+	}
+
+	function setActiveLabel(newLabel: HTMLElement) {
+		if (activeLabel) {
+			activeLabel.style.filter = og_filter
+		}
+		activeLabel = newLabel
+		og_filter = activeLabel.style.filter
+
+		activeLabel.style.filter = 'brightness(1.5)'
+	}
+
+	function wrap(str: string | number, color: string) {
+		return `<span style="color: ${color}">${str}</span>`
 	}
 
 	return {
 		destroy() {
+			globalThis.window?.removeEventListener('keydown', toggle)
+			// @ts-expect-error - I need to read up on this passive event thing ig
+			globalThis.window?.removeEventListener('pointermove', updateClientXY, { passive: true })
 			elements.forEach((element) => {
-				;(element as HTMLElement).removeEventListener('mouseenter', handleMouseEnter)
-				;(element as HTMLElement).removeEventListener('mouseleave', handleMouseLeave)
+				;(element as HTMLElement).removeEventListener('pointerenter', handlePointerEnter)
 			})
 		},
 	}
