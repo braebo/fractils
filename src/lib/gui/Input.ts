463bfe59aadb37827ebde6f207564353b967c737
Braden Wiggins <braden@fractal-hq.com>
2024-02-20 19:39:34 -0500

    [feat] smaller icon

--- src/lib/gui/Input.ts
+++ src/lib/gui/Input.ts
@@ -422,7 +422,7 @@ export class InputSlider extends Input {
 		const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
 		svg.setAttribute('width', '24')
 		svg.setAttribute('height', '24')
-		svg.setAttribute('viewBox', '0 0 24 24')
+		svg.setAttribute('viewBox', '-2 -2 28 28')
 		svg.setAttribute('stroke', 'currentColor')
 		svg.setAttribute('fill', 'none')
 		svg.setAttribute('stroke-width', '2')
