27c6f3057441055a52aa62b4cebfd7e005d0c45c
Braden Wiggins <braden@fractal-hq.com>
2024-03-07 23:11:54 -0500

    [feat]  type

--- src/lib/gui/controllers/types.ts
+++ src/lib/gui/controllers/types.ts
@@ -1,12 +1,12 @@
-import type { Input, ElementMap, InputOptions } from '../inputs/Input'
+import type { Input, ElementMap, InputOptions, ValidInputs } from '../inputs/Input'
 
 /**
  * Controller factory funtions create the DOM elements that are
  * used to control the input, and bind their change events to
- * the input's {@link Input.updateState | updateState} method.
+ * the input's {@link Input.refresh | updateState} method.
  */
 export type ControllerFactory<
 	TElement extends Element | ElementMap,
-	TInput extends Input = Input<any>,
+	TInput extends ValidInputs = ValidInputs,
 	TOptions extends InputOptions<any> = InputOptions,
 > = (input: TInput, opts: TOptions, parent?: HTMLElement) => TElement
