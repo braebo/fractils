733598596b8374eccb2459612f63ae7791dabcd5
Braden Wiggins <braden@fractal-hq.com>
2024-03-15 20:40:39 -0400

    [feat] new `mount` util

---
+++ src/lib/utils/mount.ts
@@ -0,0 +1,35 @@
+/**
+ * Appends a list of elements to one another in the order they are passed.
+ *
+ * @example
+ * ```typescript
+ * append(foo, bar, baz, qux)
+ * // is equivalent to:
+ * foo.appendChild(bar)
+ * bar.appendChild(baz)
+ * baz.appendChild(qux)
+ * ```
+ */
+export function append(...els: Element[]) {
+	let i = 0
+
+	const e = [...els]
+	/** parent */
+	let p = e.shift()
+	/** child */
+	let c = e.shift()
+
+	function mount(..._e: Element[]) {
+		if (i++ > 200) throw new Error('infinite loop?')
+
+		if (p && c) p.appendChild(c)
+
+		p = c
+		c = _e.shift()
+
+		if (c) {
+			mount(..._e)
+		}
+	}
+	mount(...e)
+}
