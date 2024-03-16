a8f37a867e0ecc80194588db71b45577f201de7b
Braden Wiggins <braden@fractal-hq.com>
2024-03-15 20:42:06 -0400

    [feat] add `autoprefixer`

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		postcss: {
+			plugins: [autoprefixer],
+		},
+	},
 })
