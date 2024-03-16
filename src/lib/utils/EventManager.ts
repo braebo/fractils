5def4ddec26154d5ec1c1c7e6fc08af5ce3447c5
Braden Wiggins <braden@fractal-hq.com>
2024-03-15 23:45:40 -0400

    [feat] new `EventManager` util

---
+++ src/lib/utils/EventManager.ts
@@ -0,0 +1,52 @@
+import { nanoid } from './nanoid'
+
+/**
+ * Represents an event manager that provides methods for adding and removing event listeners.
+ */
+export class EventManager {
+	listeners = new Map<string, () => void>()
+
+	/**
+	 * Adds an event listener to an element.
+	 * @param element - The element to add the listener to.
+	 * @param event - The event to listen for.
+	 * @param callback - The callback function to execute when the event is fired.
+	 * @param options - Optional event listener options.
+	 */
+	listen(
+		element: HTMLElement | Window | Document,
+		event: GlobalEventHandlersEventMap,
+		callback: (event: Event) => void,
+		options?: AddEventListenerOptions,
+	): void {
+		const id = nanoid()
+		element.addEventListener(event as unknown as string, callback, options)
+		this.listeners.set(id, () => {
+			element.removeEventListener(event as unknown as string, callback, options)
+		})
+	}
+
+	/**
+	 * Removes a specific listener from the event manager
+	 * without removing the listener from the element.
+	 */
+	ignore(id: string): boolean {
+		return this.listeners.delete(id)
+	}
+
+	/**
+	 * Removes all registered listeners from the
+	 * element and clears the event manager.
+	 */
+	clear(): void {
+		for (const cb of this.listeners.values()) cb()
+		this.listeners.clear()
+	}
+
+	/**
+	 * Removes all registered listeners.
+	 */
+	dispose(): void {
+		this.clear()
+	}
+}
