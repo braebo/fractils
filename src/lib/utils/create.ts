a9aeb4d5318aad02b46fe2db6cd326cc55d1b2cd
Braden Wiggins <braden@fractal-hq.com>
2024-03-15 23:50:24 -0400

    [fix] better types / better pizza

--- src/lib/utils/create.ts
+++ src/lib/utils/create.ts
@@ -17,48 +17,45 @@ export type CreateOptions = {
 	value?: string
 } & Partial<Record<keyof HTMLElement | keyof HTMLInputElement, any>>
 
-export function create<T extends HTMLElement = HTMLElement>(
-	tagnameOrElement: string,
-	options: CreateOptions,
+export function create<const K extends keyof HTMLElementTagNameMap>(
+	tagname: K,
+	options?: CreateOptions,
 	...children: HTMLElement[]
-) {
-	const el =
-		typeof tagnameOrElement === 'string'
-			? globalThis?.document?.createElement(tagnameOrElement)
-			: tagnameOrElement
+): HTMLElementTagNameMap[K] {
+	const el = globalThis?.document?.createElement(tagname)
 
-	if (options.classes) el.classList.add(...options.classes)
-	if (options.id) el.id = options.id
-	if (options.dataset) Object.assign(el.dataset, options.dataset)
-	if (options.textContent) el.textContent = options.textContent
-	if (options.innerText) el.innerText = options.innerText
-	if (options.cssText) el.style.cssText = options.cssText
-	if (options.value && el instanceof HTMLInputElement) el.value = options.value
-	if (options.type) el.setAttribute('type', options.type)
+	if (options) {
+		if (options.classes) el.classList.add(...options.classes)
+		if (options.id) el.id = options.id
+		if (options.dataset) Object.assign(el.dataset, options.dataset)
+		if (options.textContent) el.textContent = options.textContent
+		if (options.innerText) el.innerText = options.innerText
+		if (options.cssText) el.style.cssText = options.cssText
+		if (options.value && el instanceof HTMLInputElement) el.value = options.value
+		if (options.type) el.setAttribute('type', options.type)
 
-	if (options.attributes) {
-		for (const [key, value] of entries(options.attributes)) {
-			el.setAttribute(key, value)
+		if (options.attributes) {
+			for (const [key, value] of entries(options.attributes)) {
+				el.setAttribute(key, value)
+			}
 		}
-	}
 
-	if (options.style) {
-		for (const [key, value] of entries(options.style)) {
-			el.style[key] = String(value)
+		if (options.style) {
+			for (const [key, value] of entries(options.style)) {
+				el.style[key] = String(value)
+			}
 		}
-	}
 
-	if (options.variables) {
-		for (const [key, value] of entries(options.variables)) {
-			el.style.setProperty(key, String(value))
+		if (options.variables) {
+			for (const [key, value] of entries(options.variables)) {
+				el.style.setProperty(key, String(value))
+			}
 		}
+
+		if (options.parent) options.parent.appendChild(el)
 	}
 
 	for (const child of children) el.appendChild(child)
 
-	if (options.parent) options.parent.appendChild(el)
-
-	return el as unknown as T
+	return el
 }
-
-// const input = document.createElement('input')
