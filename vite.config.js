1367596985007673aac16075d30d2203f7991762
Braden Wiggins <braden@fractal-hq.com>
2024-03-20 17:36:08 -0400

    [config]  config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import { sveltekit } from '@sveltejs/kit/vite'
 import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		vanillaExtractPlugin({
+			// identifiers: ({ hash }) => `fractils_${hash}`,
+			identifiers: ({ hash, debugId }) => `fractils-${debugId}`,
+		}),
+		sveltekit(),
+	],
 	css: {
 		postcss: {
 			plugins: [autoprefixer],
