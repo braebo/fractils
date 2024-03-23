53014f458191fdb524bb829be2f7148c4d0454be
Braden Wiggins <braden@fractal-hq.com>
2024-03-23 17:57:13 -0400

    [fix] lingering  config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import { sveltekit } from '@sveltejs/kit/vite'
 import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [
-		vanillaExtractPlugin({
-			// identifiers: ({ hash }) => `fractils_${hash}`,
-			identifiers: ({ hash, debugId }) => `fractils-${debugId}`,
-		}),
-		sveltekit(),
-	],
+	plugins: [sveltekit()],
 	css: {
 		postcss: {
 			plugins: [autoprefixer],
