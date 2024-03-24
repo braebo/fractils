e629ed3fb4bd2528f85f3f8644130e788216aa74
Braden Wiggins <braden@fractal-hq.com>
2024-03-24 19:17:34 -0400

    [wip] theme testing page

---
+++ src/routes/demo/theme/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	// import * as css from '../../../lib/theme/contract.css'
+</script>
+
+<!-- <pre>{JSON.stringify(css, null, 2)}</pre> -->
+
+<div class=".fracgui-root">
+	<h1 style="color: var(--theme-a)">Dark Theme A</h1>
+	<h1>Light Theme A</h1>
+</div>
