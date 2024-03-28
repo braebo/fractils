956a3c278c858132ce2a61332171fd3edc8b07a4
Braden Wiggins <braden@fractal-hq.com>
2024-03-28 19:50:02 -0400

    [feat]  overhaul +  class

--- src/lib/utils/windowManager.ts
+++ src/lib/utils/windowManager.ts
@@ -1,30 +1,20 @@
-import type { ElementsOrSelectors } from './select'
+import type { ElementsOrSelector, ElementsOrSelectors } from './select'
 
-import { Resizable, type ResizableOptions } from './resizable'
-import { Draggable, type DragOptions } from './draggable'
+import { Resizable, RESIZABLE_DEFAULTS, type ResizableOptions } from './resizable'
+import { DRAG_DEFAULTS, Draggable, type DraggableOptions } from './draggable'
+import { resolveOpts } from '$lib/gui/shared/resolveOpts'
 import { EventManager } from './EventManager'
-import { Logger } from './logger'
 import { deepMerge } from './deepMerge'
+import { Logger } from './logger'
+import { state } from './state'
 
 export interface WindowManagerOptions {
 	/**
-	 * The base z-index value.
-	 * @default 0
-	 */
-	zFloor: number
-
-	/**
-	 * Restores a selected window's z-index immediately upon release.
-	 * @default false
-	 */
-	preserveZ: boolean
-
-	/**
 	 * Whether to make windows draggable. Can be a boolean, or your own
-	 * {@link DragOptions}.  Set to `false` to disable dragging.
+	 * {@link DraggableOptions}.  Set to `false` to disable dragging.
 	 * @default true
 	 */
-	draggable: boolean | Partial<DragOptions>
+	draggable: boolean | Partial<DraggableOptions>
 
 	/**
 	 * Whether to make windows resizable. Can be a boolean, or your own
@@ -32,12 +22,6 @@ export interface WindowManagerOptions {
 	 * @default true
 	 */
 	resizable: boolean | Partial<ResizableOptions>
-
-	// /**
-	//  * Animation options for window selection.
-	//  * @default { scale: 1., duration: 75 }
-	//  */
-	// animation: false | Partial<AnimationOptions>
 
 	/**
 	 * Element's or selectors which will act as collision obstacles for the element.
@@ -49,35 +33,61 @@ export interface WindowManagerOptions {
 	 * Element's or selectors which will act as bounds obstacles for the element.
 	 * @default ''
 	 */
-	bounds: ElementsOrSelectors
+	bounds: ElementsOrSelector
+
+	/**
+	 * The base z-index value.
+	 * @default 0
+	 */
+	zFloor: number
+
+	/**
+	 * Restores a selected window's z-index immediately upon release.
+	 * @default false
+	 */
+	preserveZ: boolean
 }
 
-interface AnimationOptions {
-	scale: number
-	duration: number
+export interface WindowManagerStorageOptions {
+	/**
+	 * Prefix to use for localStorage keys.
+	 * @default "fractils::window-manager"
+	 */
+	key: string
+
+	/**
+	 * Whether to persist the size.
+	 * @default true
+	 */
+	size?: boolean
+
+	/**
+	 * Whether to persist the position when windowManager is enabled.
+	 * @default true
+	 */
+	position?: boolean
+
+	/**
+	 * How long to debounce writes to localStorage (0 to disable).
+	 * @default 50
+	 */
+	debounce?: number
 }
 
-export const WINDOWMANAGER_DEFAULTS: WindowManagerOptions = {
+export const WINDOWMANAGER_DEFAULTS = {
+	resizable: {
+		...RESIZABLE_DEFAULTS,
+		// localStorageKey: 'fractils::window-manager::resizable',
+	},
+	draggable: {
+		...DRAG_DEFAULTS,
+		// localStorageKey: 'fractils::window-manager::draggable',
+	},
 	zFloor: 0,
 	preserveZ: false,
-	resizable: true,
-	draggable: true,
-	// animation: {
-	// 	duration: 75,
-	// 	scale: 1,
-	// } as const,
-	obstacles: undefined,
 	bounds: undefined,
-} as const
-
-/**
- * A single window in a window manager.
- */
-interface WindowInstance {
-	element: HTMLElement
-	draggableInstance?: Draggable
-	resizableInstance?: Resizable
-}
+	obstacles: undefined,
+} as const satisfies WindowManagerOptions
 
 /**
  * Manages the z-index of multiple elements to ensure
@@ -90,12 +100,6 @@ export class WindowManager {
 	#log = new Logger('WindowManager', { fg: 'lightseagreen' })
 	#evm = new EventManager()
 
-	get draggableOptions() {
-		return this.opts.draggable as DragOptions | false
-	}
-	get resizableOptions() {
-		return this.opts.resizable as ResizableOptions | false
-	}
 	// get animationOptions() {
 	// 	return this.opts.animation as AnimationOptions | false
 	// }
@@ -105,66 +109,91 @@ export class WindowManager {
 		this.#log.fn('constructor').info({ opts: this.opts, this: this })
 	}
 
-	add = (node: HTMLElement, options?: Partial<WindowManagerOptions>) => {
-		if (options?.preserveZ) node.dataset.keepZ = 'true'
+	add = (node: HTMLElement, options?: Partial<WindowInstanceOptions>) => {
+		// const instanceOpts: WindowInstanceOptions = {
+		// 	preserveZ: this.opts.preserveZ,
+		// 	resizable: false,
+		// 	draggable: false,
+		// 	bounds: this.opts.bounds,
+		// }
 
-		const instance: WindowInstance = {
-			element: node,
-			resizableInstance: undefined,
-			draggableInstance: undefined,
-		}
+		// if (options?.draggable !== false) {
+		// 	const dragObstacles = this.#resolveObstacles(options?.draggable, this.opts.draggable)
+		// 	if (dragObstacles) {
+		// 		instanceOpts.draggable = Object.assign({}, this.opts.draggable, dragObstacles)
+		// 	}
+		// }
 
-		if (this.draggableOptions) {
-			const obstacles = options?.obstacles ?? this.draggableOptions.obstacles
-			// Order of precedence: options.draggable.obstacles > options.obstacles > this.opts.obstacles
-			const opts = Object.assign({}, this.draggableOptions, { obstacles }, options?.draggable)
-			instance.draggableInstance = new Draggable(node, opts)
-		}
+		// if (options?.resizable !== false) {
+		// 	const resizeObstacles = this.#resolveObstacles(options?.resizable, this.opts.resizable)
+		// 	if (resizeObstacles) {
+		// 		instanceOpts.resizable = Object.assign({}, this.opts.resizable, resizeObstacles)
+		// 	}
+		// }
 
-		if (this.resizableOptions) {
-			const obstacles = options?.obstacles ?? this.opts.obstacles
-			// Order of precedence: options.resizable.obstacles > options.obstacles > this.opts.obstacles
-			const opts = Object.assign({}, this.resizableOptions, { obstacles }, options?.resizable)
-			instance.resizableInstance = new Resizable(node, opts)
-		}
+		const instanceOpts = this.#resolveOptions(options) as WindowInstanceOptions
+		this.#log.fn('add').info({ node, options, instanceOpts })
 
-		this.windows.push(instance)
+		this.windows.push(new WindowInstance(node, instanceOpts))
 
 		this.#evm.listen(node, 'grab', this.select)
 
-		return {
-			destroy: () => {
-				this.dispose()
-			},
-		}
+		return this
 	}
+
+	// #resolveObstacles<T extends 'draggable' | 'resizable'>(
+	// 	localOverride: T extends 'draggable'
+	// 		? WindowInstanceOptions['draggable'] | boolean | undefined
+	// 		: WindowInstanceOptions['resizable'] | boolean | undefined,
+	// 	localBase: T extends 'draggable'
+	// 		? WindowManagerOptions['draggable'] | undefined
+	// 		: WindowManagerOptions['resizable'] | undefined,
+	// ): { obstacles: ElementsOrSelectors } {
+	// 	let obstacles: ElementsOrSelectors = []
+
+	// 	if (typeof localOverride === 'object' && localOverride?.obstacles) {
+	// 		obstacles = localOverride.obstacles
+	// 	} else if (typeof localBase === 'object' && localBase.obstacles) {
+	// 		obstacles = this.opts.obstacles
+	// 	} else if (localOverride === true) {
+	// 		obstacles = this.opts.obstacles
+	// 	} else if (localOverride === false) {
+	// 		obstacles = []
+	// 	}
+
+	// 	return { obstacles }
+	// }
 
 	applyZ() {
 		for (let i = 1; i < this.windows.length; i++) {
-			this.windows[i].element.style.setProperty('z-index', String(this.opts.zFloor + i))
+			this.windows[i].node.style.setProperty('z-index', String(this.opts.zFloor + i))
 		}
 
 		return this
 	}
 
 	select = (e: PointerEvent) => {
-		const node = e.currentTarget as HTMLElement
+		const target_node = e.currentTarget as HTMLElement
 
-		const instance = this.windows.find(({ element }) => element === node)
+		const instance = this.windows.find(({ node }) => node === target_node)
 
 		if (!instance) {
-			throw new Error('Unable to resolve instance from selected node: ' + node)
+			throw new Error('Unable to resolve instance from selected node: ' + target_node)
 		}
 
 		// this.#animate(node)
 
-		const initialZ = node.style.getPropertyValue('z-index')
-		node.style.setProperty('z-index', String(this.opts.zFloor + this.windows.length))
+		const initialZ = target_node.style.getPropertyValue('z-index')
+		target_node.style.setProperty('z-index', String(this.opts.zFloor + this.windows.length))
 
-		if (node.dataset.keepZ === 'true' || this.opts.preserveZ) {
-			addEventListener('pointerup', () => node.style.setProperty('z-index', initialZ), {
-				once: true,
-			})
+		if (target_node.dataset.keepZ === 'true' || this.opts.preserveZ) {
+			addEventListener(
+				'pointerup',
+				() => target_node.style.setProperty('z-index', initialZ),
+				{
+					once: true,
+				},
+			)
 		} else {
 			this.windows = this.windows.filter(i => i !== instance)
 			this.windows.push(instance)
@@ -172,67 +201,86 @@ export class WindowManager {
 		}
 	}
 
-	// #animate = (node: HTMLElement) => {
-	// 	if (!this.animationOptions) return
-
-	// 	const scale = parseFloat(node.style.getPropertyValue('scale')) || 1
-
-	// 	const animOpts = {
-	// 		duration: this.animationOptions.duration,
-	// 		easing: 'ease-out',
-	// 		fill: 'forwards',
-	// 	} as const
-
-	// 	node.animate([{ scale }, { scale: scale - 1 + this.animationOptions.scale }], animOpts)
-
-	// 	window.addEventListener('pointerup', () => node.animate([{ scale }], animOpts), {
-	// 		once: true,
-	// 	})
-	// }
-
 	#resolveOptions(options?: Partial<WindowManagerOptions>): WindowManagerOptions {
-		// const opts = { ...WINDOWMANAGER_DEFAULTS, ...options }
 		this.#log.fn('#resolveOptions').info(options)
-		const opts = deepMerge(WINDOWMANAGER_DEFAULTS, options)
+		const opts = deepMerge<WindowManagerOptions>(WINDOWMANAGER_DEFAULTS, options)
 
-
-		// if (options?.animation === false) {
-		// 	opts.animation = false
-		// } else {
-		// 	opts.animation = {
-		// 		...WINDOWMANAGER_DEFAULTS.animation,
-		// 		...(options?.animation as AnimationOptions),
-		// 	}
-		// }
-
-		opts.draggable =
-			typeof options?.draggable === 'object'
-				? options.draggable
-				: options?.draggable === true
-					? WINDOWMANAGER_DEFAULTS.draggable
-					: false
-
-		opts.resizable =
-			typeof options?.resizable === 'object'
-				? options.resizable
-				: options?.resizable === true
-					? WINDOWMANAGER_DEFAULTS.resizable
-					: false
+		opts.draggable = resolveOpts(options?.draggable, WINDOWMANAGER_DEFAULTS.draggable)
+		opts.resizable = resolveOpts(options?.resizable, WINDOWMANAGER_DEFAULTS.resizable)
 
 		// Add any obstacles to both the draggable and resizable options.
 		if (opts.obstacles) {
 			if (opts.draggable) {
-				;(opts.draggable as DragOptions).obstacles = opts.obstacles
+				;(opts.draggable as DraggableOptions).obstacles = opts.obstacles
 			}
 			if (opts.resizable) {
 				;(opts.resizable as ResizableOptions).obstacles = opts.obstacles
 			}
 		}
 
+		if (opts.bounds) {
+			if (opts.draggable) {
+				;(opts.draggable as DraggableOptions).bounds = opts.bounds
+			}
+			if (opts.resizable) {
+				;(opts.resizable as ResizableOptions).bounds = opts.bounds
+			}
+		}
+
+		// Pass on storage options, and add a suffix to the key.
+		// const storageOpts = resolveOpts(options?.storage, WINDOWMANAGER_DEFAULTS.storage)
+		// if (storageOpts) opts.storage = storageOpts
+
 		return opts
 	}
 
 	dispose() {
 		this.#evm.dispose()
+		for (const instance of this.windows) {
+			instance.resizableInstance?.dispose()
+			instance.draggableInstance?.dispose()
+		}
+	}
+}
+
+interface WindowInstanceOptions {
+	draggable: DraggableOptions | boolean
+	resizable: ResizableOptions | boolean
+	bounds?: WindowManagerOptions['bounds']
+	obstacles?: WindowManagerOptions['obstacles']
+	preserveZ?: WindowManagerOptions['preserveZ']
+}
+
+/**
+ * A single window in a window manager.
+ */
+class WindowInstance {
+	draggableInstance?: Draggable
+	resizableInstance?: Resizable
+
+	position = state({ x: 0, y: 0 })
+	size = state({ width: 0, height: 0 })
+
+	constructor(
+		public node: HTMLElement,
+		options: WindowInstanceOptions,
+	) {
+		if (options?.preserveZ) node.dataset.keepZ = 'true'
+
+		const dragOpts = resolveOpts(options.draggable, DRAG_DEFAULTS)
+		const resizeOpts = resolveOpts(options.resizable, RESIZABLE_DEFAULTS)
+
+		if (dragOpts) {
+			this.draggableInstance = new Draggable(node, dragOpts)
+		}
+
+		if (resizeOpts) {
+			this.resizableInstance = new Resizable(node, resizeOpts)
+		}
+	}
+
+	dispose() {
+		this.resizableInstance?.dispose()
+		this.draggableInstance?.dispose()
 	}
 }
