3a9aa6e9ca3fb7d258d2e610716390c82b5e8f06
Braden Wiggins <braden@fractal-hq.com>
2024-03-29 20:22:55 -0400

    [feat] more robust / documented

--- src/lib/utils/deepMerge.ts
+++ src/lib/utils/deepMerge.ts
@@ -1,23 +1,38 @@
 import { isObject } from './is'
 
-export function deepMerge<T extends {} = {}, U extends {} | undefined = {}>(
-	target: T,
-	...sources: U[]
-): T & U {
+/**
+ * Deep merges objects together, with some special rules:
+ * - Subsequent objects must be partials of the first object.
+ * - Arrays are concatenated and de-duplicated.
+ * - Objects are recursively merged.
+ * - `true` is replaced by objects, but not `false`.
+ * - An object is never replaced with `true`, `false`, or `undefined`.
+ * - The original objects are not mutated.
+ */
+export function deepMerge<
+	T extends {} = {},
+	U extends Partial<T> | undefined = Partial<T> | undefined,
+>(target: T, ...sources: U[]): T & U {
 	return sources.reduce<T & U>(
-		(acc, source) => {
-			if (!source) return acc
+		(acc, curr) => {
+			if (!curr) return acc
 
-			Object.keys(source).forEach(key => {
-				const accValue = acc[key]
-				const sourceValue = source[key]
+			Object.keys(curr).forEach(key => {
+				const v = acc[key]
+				const newV = curr[key]
 
-				if (Array.isArray(accValue) && Array.isArray(sourceValue)) {
-					acc[key] = accValue.concat(sourceValue)
-				} else if (isObject(accValue) && isObject(sourceValue)) {
-					acc[key] = deepMerge({ ...accValue }, sourceValue)
-				} else if (sourceValue !== true) {
-					acc[key] = sourceValue
+				if (Array.isArray(v) && Array.isArray(newV)) {
+					acc[key] = [...new Set([...v, ...newV])]
+				} else if (isObject(v)) {
+					if (newV !== true) {
+						if (isObject(newV)) {
+							acc[key] = deepMerge({ ...v }, newV)
+						} else if (newV === false) {
+							acc[key] = newV
+						}
+					}
+				} else if (newV !== undefined) {
+					acc[key] = newV
 				}
 			})
 			return acc
