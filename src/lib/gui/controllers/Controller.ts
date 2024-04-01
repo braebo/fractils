aed05dc1fa89f671e1ebf145167a08ac07ea0c0d
Braden Wiggins <braden@fractal-hq.com>
2024-03-31 22:17:26 -0400

    [feat] new `Controller` class

--- src/lib/gui/controllers/Controller.ts
+++ src/lib/gui/controllers/Controller.ts
@@ -1,12 +1,79 @@
-import type { ElementMap, Input } from '../inputs/Input'
+import type { ElementMap } from '../inputs/Input'
 
-export abstract class Controller<
-	TInput extends Input<any> = Input<any>,
-	TElements extends ElementMap = ElementMap,
-> {
-	elements = {} as TElements
+import { EventManager } from '../../utils/EventManager'
+import { toFn } from '../shared/toFn'
 
-	constructor(public input: TInput) {}
+export abstract class Controller<TValue, TElements extends ElementMap = ElementMap> {
+	#evm = new EventManager()
 
-	refresh() {}
+	/**
+	 * All elements created by the controller.
+	 */
+	abstract elements: TElements
+	/**
+	 * Usually the 'container' element for the controller if it has one.
+	 * Otherwise, the main element of the controller if there's only one.
+	 */
+	abstract element: Element
+
+	/**
+	 * Whether the controller is disabled.  A function can be used to
+	 * dynamically determine the disabled state.
+	 */
+	#disabled: () => boolean
+
+	/**
+	 * Whether the controller has been disposed.
+	 */
+	#disposed = false
+
+	constructor(
+		opts: Record<string, any> & {
+			disabled: boolean | (() => boolean)
+		},
+	) {
+		this.#disabled = toFn(opts.disabled)
+	}
+
+	get disabled(): boolean {
+		return this.#disabled()
+	}
+	set disabled(v: boolean | (() => boolean)) {
+		this.#disabled = toFn(v)
+		this.#disabled() ? this.disable() : this.enable()
+	}
+
+	get disposed() {
+		return this.#disposed
+	}
+
+	abstract refresh: () => this
+
+	listen = this.#evm.listen.bind(this.#evm)
+
+	enable() {
+		this.#disabled = toFn(false)
+	}
+	disable() {
+		this.#disabled = toFn(true)
+	}
+
+	#onChangeListeners = new Set<(...args: any[]) => void>()
+	onChange(cb: (value: TValue) => void) {
+		this.#onChangeListeners.add(cb)
+		return this
+	}
+	callOnChange = (value: TValue) => {
+		this.element.dispatchEvent(new CustomEvent('change', { detail: value }))
+		for (const cb of this.#onChangeListeners) {
+			cb(value)
+		}
+		return this
+	}
+
+	dispose() {
+		this.#disposed = true
+		this.#onChangeListeners.clear()
+		this.#evm.dispose()
+	}
 }
