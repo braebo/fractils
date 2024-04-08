09108fd7d42fd1cb4fe98c3f9c13ab2719211133
Braden Wiggins <braden@fractal-hq.com>
2024-04-08 18:28:01 -0400

    [feat] updated themes

--- src/lib/themer/themes/default.ts
+++ src/lib/themer/themes/default.ts
@@ -4,23 +4,28 @@ import { resolveTheme } from '../resolveTheme'
 
 export default resolveTheme({
 	title: 'default',
-	color: {
-		base: {
-			'theme-a': '#00bcd4',
-			'theme-b': '#f8d2c9',
-			'theme-c': '#ba788a',
-			'dark-a': '#0B0B11',
-			'dark-b': '#1b1d29',
-			'dark-c': '#46465e',
-			'dark-d': '#55566a',
-			'dark-e': '#787b89',
-			'light-a': '#ffffff',
-			'light-b': '#c9ccd7',
-			'light-c': '#a9adba',
-			'light-d': '#777D8F',
-			'light-e': '#5F6377',
+	prefix: 'fracgui',
+	vars: {
+		color: {
+			base: {
+				'theme-a': '#00bcd4',
+				'theme-b': '#f8d2c9',
+				'theme-c': '#ba788a',
+				'dark-a': '#0B0B11',
+				'dark-b': '#1b1d29',
+				'dark-c': '#46465e',
+				'dark-d': '#55566a',
+				'dark-e': '#787b89',
+				'light-a': '#ffffff',
+				'light-b': '#c9ccd7',
+				'light-c': '#a9adba',
+				'light-d': '#777D8F',
+				'light-e': '#5F6377',
+			},
+			dark: {},
+			light: {
+				'theme-a': '#00bcd4',
+			},
 		},
-		dark: {},
-		light: {},
 	},
 } as const satisfies ThemeDefinition)
--- src/lib/themer/themes/flat.ts
+++ src/lib/themer/themes/flat.ts
@@ -4,23 +4,26 @@ import { resolveTheme } from '../resolveTheme'
 
 export default resolveTheme({
 	title: 'flat',
-	color: {
-		base: {
-			'theme-a': '#00bcd4',
-			'theme-b': '#f8d2c9',
-			'theme-c': '#ba788a',
-			'dark-a': '#0B0B11',
-			'dark-b': '#15161D',
-			'dark-c': '#1F202D',
-			'dark-d': '#353746',
-			'dark-e': '#474A5B',
-			'light-a': '#ffffff',
-			'light-b': '#dfe1e9',
-			'light-c': '#BABECA',
-			'light-d': '#777D8F',
-			'light-e': '#5F6377',
+	prefix: 'fracgui',
+	vars: {
+		color: {
+			base: {
+				'theme-a': '#00bcd4',
+				'theme-b': '#f8d2c9',
+				'theme-c': '#ba788a',
+				'dark-a': '#0B0B11',
+				'dark-b': '#15161D',
+				'dark-c': '#1F202D',
+				'dark-d': '#353746',
+				'dark-e': '#474A5B',
+				'light-a': '#ffffff',
+				'light-b': '#dfe1e9',
+				'light-c': '#BABECA',
+				'light-d': '#777D8F',
+				'light-e': '#5F6377',
+			},
+			dark: {},
+			light: {},
 		},
-		dark: {},
-		light: {},
 	},
 } as const satisfies ThemeDefinition)
