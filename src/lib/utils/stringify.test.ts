5479989c8120c5bb97266b7735d61806d7fb399e
Braden Wiggins <braden@fractal-hq.com>
2024-04-11 22:15:15 -0400

    [fix] use `expect` from `vitest`

--- src/lib/utils/stringify.test.ts
+++ src/lib/utils/stringify.test.ts
@@ -1,6 +1,5 @@
-import { strict as assert } from 'assert';
-import { stringify } from './stringify';
-import { test } from 'vitest';
+import { stringify } from './stringify'
+import { test, expect } from 'vitest'
 
 test('stringify', () => {
 	const obj = {
@@ -13,13 +12,12 @@ test('stringify', () => {
 		array: [1, 2, 3],
 		function: () => {},
 		get circular() {
-			return this;
+			return this
 		},
-	};
-	obj.nested.a.b = obj.nested;
+	}
+	obj.nested.a.b = obj.nested
 
-	const result = stringify(obj, 2);
-	console.log('\nresult:\n', result);
+	const result = stringify(obj, 2)
 
 	const expected = `{
   "number": 1,
@@ -39,7 +37,7 @@ test('stringify', () => {
   ],
   "function": "function",
   "circular": "[Circular ~]"
-}`;
+}`
 
-	assert.equal(result, expected);
-});
+	expect(result).toBe(expected)
+})
