6e5273d2847da606c6a7a9cc0eae20b1875276ae
Braden Wiggins <braden@fractal-hq.com>
2024-04-11 21:26:20 -0400

    [feat] `themer`/`resolveTheme` overhaul

--- src/lib/themer/resolveTheme.ts
+++ src/lib/themer/resolveTheme.ts
@@ -2,41 +2,44 @@ import { deepMerge } from '$lib/utils/deepMerge'
 import type { ExtendedVars, Theme, ThemeDefinition } from './types'
 
 export function resolveTheme(def: ThemeDefinition, vars: ExtendedVars = {}) {
-	return {
+	const color = def.resolved
+		? {}
+		: {
+				base: def.vars.color.base,
+				dark: {
+					'bg-a': def.vars.color.dark?.['bg-a'] || def.vars.color.base['dark-a'],
+					'bg-b': def.vars.color.dark?.['bg-b'] || def.vars.color.base['dark-b'],
+					'bg-c': def.vars.color.dark?.['bg-c'] || def.vars.color.base['dark-c'],
+					'bg-d': def.vars.color.dark?.['bg-d'] || def.vars.color.base['dark-d'],
+					'bg-e': def.vars.color.dark?.['bg-e'] || def.vars.color.base['dark-e'],
+					'fg-a': def.vars.color.dark?.['fg-a'] || def.vars.color.base['light-a'],
+					'fg-b': def.vars.color.dark?.['fg-b'] || def.vars.color.base['light-b'],
+					'fg-c': def.vars.color.dark?.['fg-c'] || def.vars.color.base['light-c'],
+					'fg-d': def.vars.color.dark?.['fg-d'] || def.vars.color.base['light-d'],
+					'fg-e': def.vars.color.dark?.['fg-e'] || def.vars.color.base['light-e'],
+					...def.vars.color.dark,
+				},
+				light: {
+					'bg-a': def.vars.color.light?.['bg-a'] || def.vars.color.base['light-a'],
+					'bg-b': def.vars.color.light?.['bg-b'] || def.vars.color.base['light-b'],
+					'bg-c': def.vars.color.light?.['bg-c'] || def.vars.color.base['light-c'],
+					'bg-d': def.vars.color.light?.['bg-d'] || def.vars.color.base['light-d'],
+					'bg-e': def.vars.color.light?.['bg-e'] || def.vars.color.base['light-e'],
+					'fg-a': def.vars.color.light?.['fg-a'] || def.vars.color.base['dark-a'],
+					'fg-b': def.vars.color.light?.['fg-b'] || def.vars.color.base['dark-b'],
+					'fg-c': def.vars.color.light?.['fg-c'] || def.vars.color.base['dark-c'],
+					'fg-d': def.vars.color.light?.['fg-d'] || def.vars.color.base['dark-d'],
+					'fg-e': def.vars.color.light?.['fg-e'] || def.vars.color.base['dark-e'],
+					...def.vars.color.light,
+				},
+			}
+
+	const theme: Theme = {
 		title: def.title,
 		prefix: def.prefix || 'fractils',
-		vars: deepMerge(
-			{
-				color: {
-					base: def.vars.color.base,
-					dark: {
-						'bg-a': def.vars.color.dark?.['bg-a'] || def.vars.color.base['dark-a'],
-						'bg-b': def.vars.color.dark?.['bg-b'] || def.vars.color.base['dark-b'],
-						'bg-c': def.vars.color.dark?.['bg-c'] || def.vars.color.base['dark-c'],
-						'bg-d': def.vars.color.dark?.['bg-d'] || def.vars.color.base['dark-d'],
-						'bg-e': def.vars.color.dark?.['bg-e'] || def.vars.color.base['dark-e'],
-						'fg-a': def.vars.color.dark?.['fg-a'] || def.vars.color.base['light-a'],
-						'fg-b': def.vars.color.dark?.['fg-b'] || def.vars.color.base['light-b'],
-						'fg-c': def.vars.color.dark?.['fg-c'] || def.vars.color.base['light-c'],
-						'fg-d': def.vars.color.dark?.['fg-d'] || def.vars.color.base['light-d'],
-						'fg-e': def.vars.color.dark?.['fg-e'] || def.vars.color.base['light-e'],
-					},
-					light: {
-						'bg-a': def.vars.color.light?.['bg-a'] || def.vars.color.base['light-a'],
-						'bg-b': def.vars.color.light?.['bg-b'] || def.vars.color.base['light-b'],
-						'bg-c': def.vars.color.light?.['bg-c'] || def.vars.color.base['light-c'],
-						'bg-d': def.vars.color.light?.['bg-d'] || def.vars.color.base['light-d'],
-						'bg-e': def.vars.color.light?.['bg-e'] || def.vars.color.base['light-e'],
-						'fg-a': def.vars.color.light?.['fg-a'] || def.vars.color.base['dark-a'],
-						'fg-b': def.vars.color.light?.['fg-b'] || def.vars.color.base['dark-b'],
-						'fg-c': def.vars.color.light?.['fg-c'] || def.vars.color.base['dark-c'],
-						'fg-d': def.vars.color.light?.['fg-d'] || def.vars.color.base['dark-d'],
-						'fg-e': def.vars.color.light?.['fg-e'] || def.vars.color.base['dark-e'],
-					},
-				},
-			},
-			def.vars as ExtendedVars,
-			vars,
-		) as Theme['vars'],
-	} as const satisfies Theme
+		vars: deepMerge({ color }, vars, def.vars) as Theme['vars'],
+		resolved: true,
+	}
+
+	return theme
 }
--- src/lib/themer/types.ts
+++ src/lib/themer/types.ts
@@ -2,6 +2,13 @@ export type ThemeTitle = string
 export type ThemeVariant = 'light' | 'dark'
 export type ThemeMode = ThemeVariant | 'system'
 
+/**
+ * A collection of css custom properties that can be added to {@link ThemeDefinition.vars | theme vars}.
+ *
+ * @remarks Defining `base`, then adding mode-specific overrides, is the intended
+ * pattern for defining themes, as `dark` and `light` are typically an
+ * optional `Partial` of `base`.
+ */
 export interface VariableDefinition {
 	/** Variables applied to all modes. */
 	base: Record<string, string>
@@ -11,44 +18,46 @@ export interface VariableDefinition {
 	light: Record<string, string>
 }
 
-export interface VariableDefinition {
-	base: Record<string, string>
-	dark: Record<string, string>
-	light: Record<string, string>
+/**
+ * All themes come with a default color definition that can be overridden with partials.
+ *
+ * All shades have light and dark variants, allowing all other colors to adapt to the mode.
+ *
+ * The `light` and `dark` variables are automatically generated from the `base` colors
+ * if not overridden manually.
+ */
+export interface ColorDefinition extends VariableDefinition {
+	base: BaseColors
+	dark: Partial<ModeColors> & Record<string, string>
+	light: Partial<ModeColors> & Record<string, string>
+}
+
+export interface ColorTheme extends VariableDefinition {
+	base: BaseColors
+	dark: ModeColors & Record<string, string>
+	light: ModeColors & Record<string, string>
 }
 
 /**
- * All themes come with a default color definition that can be overridden with partials.
- * All shades have light and dark variants, allowing all other colors to adapt to the mode.
+ * Arbitrary variables that can be added to a theme.
  */
-export interface ColorDefinition {
-	color: {
-		base: BaseColors
-		dark: Partial<ModeColors> & Record<string, string>
-		light: Partial<ModeColors> & Record<string, string>
-	}
-}
-
-export interface ColorTheme {
-	color: {
-		base: BaseColors
-		dark: ModeColors & Record<string, string>
-		light: ModeColors & Record<string, string>
-	}
-}
-
 export type ExtendedVars = {
-	// [key: string]: Partial<VariableDefinition> & { base: Record<string, string> }
 	[key: string]: Partial<VariableDefinition>
 }
 
 /**
  * The minimum required definition for a theme.
  */
-export type ThemeDefinition<T extends ExtendedVars = ColorDefinition & Record<string, any>> = {
+export type ThemeDefinition<T extends ExtendedVars = Record<string, any>> = {
 	title: string
 	prefix?: string
-	vars: T
+	vars: T & { color: ColorDefinition }
+	/**
+	 * Whether the theme dark/light modes have been generated
+	 * from the base colors, and should not be regenerated.
+	 * @internal
+	 */
+	resolved?: boolean
 }
 
 /**
@@ -57,9 +66,15 @@ export type ThemeDefinition<T extends ExtendedVars = ColorDefinition & Record<st
 export type Theme<T extends ExtendedVars = Record<string, any>> = {
 	title: string
 	prefix: string
-	vars: ColorTheme & {
+	vars: { color: ColorTheme } & {
 		[key in keyof T]: VariableDefinition
 	}
+	/**
+	 * Whether the theme dark/light modes have been generated
+	 * from the base colors, and should not be regenerated.
+	 * @internal
+	 */
+	resolved?: boolean
 }
 
 export type ThemeColors = BaseColors & ModeColors
