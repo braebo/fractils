9e46819a5d27580875a683e66a59c2121bd5971b
Braden Wiggins <braden@fractal-hq.com>
2024-04-18 13:56:06 -0400

    [feat] add `ElementOrSelector` support to `place`

---
+++ src/lib/dom/place.bench.ts
@@ -0,0 +1,68 @@
+import type { Placement, PlacementOptions, Vec2 } from './place'
+
+import { describe, bench } from 'vitest'
+import { place_old } from '../gui/place_old'
+import { place } from './place'
+
+/**
+ * @fileoverview
+ * Just a simple benchmark to compare the performance
+ * of the old place function with the new one.
+ *
+ * Results:
+ * - place      0.3s
+ * - place_old  5.8s
+ */
+
+const size = 10
+
+const bound = { x: 0, y: 0, width: 100, height: 100 }
+const margin = 10
+
+const testObj: Record<Placement, Vec2> = {
+	center: { x: bound.width / 2 - size / 2, y: bound.height / 2 - size / 2 },
+	'top-left': { x: margin, y: margin },
+	'top-center': { x: bound.width / 2 - size / 2, y: margin },
+	'top-right': { x: bound.width - size - margin, y: margin },
+	'bottom-left': { x: margin, y: bound.height - size - margin },
+	'bottom-center': { x: bound.width / 2 - size / 2, y: bound.height - size - margin },
+	'bottom-right': { x: bound.width - size - margin, y: bound.height - size - margin },
+	'left-top': { x: margin, y: margin },
+	'left-center': { x: margin, y: bound.height / 2 - size / 2 },
+	'left-bottom': { x: margin, y: bound.height - size - margin },
+	'right-top': { x: bound.width - size - margin, y: margin },
+	'right-center': { x: bound.width - size - margin, y: bound.height / 2 - size / 2 },
+	'right-bottom': { x: bound.width - size - margin, y: bound.height - size - margin },
+	'center-top': { x: bound.width / 2 - size / 2, y: margin },
+	'center-bottom': { x: bound.width / 2 - size / 2, y: bound.height - size - margin },
+	'center-left': { x: margin, y: bound.height / 2 - size / 2 },
+	'center-right': { x: bound.width - size - margin, y: bound.height / 2 - size / 2 },
+	'center-center': { x: bound.width / 2 - size / 2, y: bound.height / 2 - size / 2 },
+}
+
+const testOptions: { placement: Placement; options: PlacementOptions; expected: Vec2 }[] =
+	Object.entries(testObj).map(([placement, expected]) => ({
+		placement: placement as Placement,
+		options: { bounds: bound, margin },
+		expected,
+	}))
+
+describe('place', () => {
+	const node = document.createElement('div')
+
+	bench('place_old', () => {
+		for (const { placement } of testOptions) {
+			const result = place_old(node, placement)
+			result.x = Math.round(result.x)
+			result.y = Math.round(result.y)
+		}
+	})
+
+	bench('place', () => {
+		for (const { placement, options } of testOptions) {
+			const result = place(node.getBoundingClientRect(), placement, options)
+			result.x = Math.round(result.x)
+			result.y = Math.round(result.y)
+		}
+	})
+})
---
+++ src/lib/dom/place.ts
@@ -0,0 +1,116 @@
+import { select, type ElementsOrSelector } from '$lib/utils/select'
+
+export type Placement = 'center' | `${TBC}-${LRC}` | `${LRC}-${TBC}`
+
+type LeftRight = 'left' | 'right'
+type LRC = LeftRight | 'center'
+
+type TopBottom = 'top' | 'bottom'
+type TBC = TopBottom | 'center'
+
+type VirtualRect = Record<string, any> & {
+	x: number
+	y: number
+	width: number
+	height: number
+}
+
+export type Vec2 = { x: number; y: number }
+
+export type PlacementOptions = Parameters<typeof place>[2]
+
+/**
+ * Determines the x and y position of an element relative to
+ * a bounding box based on a given {@link Placement} string.
+ * Optional {@link PlacementOptions} can be provided to specify
+ * the bounding box and a margin.
+ *
+ * @param node - The element to place.
+ * @param placement - The {@link Placement} string.
+ * @param options - The {@link PlacementOptions}.
+ * @param options.bounds - The bounding box to place the element within.
+ * @param options.margin - The margin in pixels to apply to the placement.
+ *
+ * @example
+ * ```ts
+ * const { x, y } = place(node, 'top-right', { bounds: window, margin: 10 })
+ * ```
+ */
+export function place(
+	node:
+		| DOMRect
+		| VirtualRect
+		| (Record<string, any> & { width: number; height: number })
+		| ElementsOrSelector,
+	placement = 'top-right',
+	options?: {
+		/**
+		 * The bounding box to place the element within.  Can be a
+		 * DOMRect, custom {@link VirtualRect}, or `'window'`.
+		 * @default 'window'
+		 */
+		bounds?: DOMRect | VirtualRect | 'window' | ElementsOrSelector
+		/**
+		 * The margin in pixels to apply to the placement.  Can be a number
+		 * to apply the same margin to both x and y, or an object with x
+		 * and y properties to apply different margins to each axis.
+		 * @default 16
+		 */
+		margin?: number | Vec2
+	},
+): Vec2 {
+	const { bounds, margin } = Object.assign(
+		{
+			bounds: undefined as VirtualRect | undefined,
+			margin: 10 as number | Vec2,
+		},
+		options,
+	)
+
+	node =
+		typeof node === 'string'
+			? select(node)[0]?.getBoundingClientRect()
+			: node instanceof Element
+				? node.getBoundingClientRect()
+				: node
+
+	if (!node) throw new Error('Invalid node: ' + node)
+
+	const b =
+		bounds === 'window' && typeof window !== 'undefined'
+			? { x: 0, y: 0, width: window.innerWidth, height: window.innerHeight }
+			: typeof bounds === 'string'
+				? select(bounds)[0]?.getBoundingClientRect()
+				: bounds ?? { x: 0, y: 0, width: 100, height: 100 }
+	
+	if (!b) throw new Error('Invalid bounds: ' + bounds)
+
+	const m = typeof margin === 'number' ? { x: margin, y: margin } : margin
+
+	if (!('x' in m) || !('y' in m)) {
+		throw new Error('Invalid margin: ' + JSON.stringify(m))
+	}
+
+	// prettier-ignore
+	switch (placement) {
+		case ('center'):
+		case ('center-center'): return { x: b.width / 2 - node.width / 2, y: b.height / 2 - node.height / 2 }
+		case ('top-left'):
+		case ('left-top'): return { x: m.x, y: m.y }
+		case ('top-center'):
+		case ('center-top'): return { x: b.width / 2 - node.width / 2, y: m.y }
+		case ('top-right'):
+		case ('right-top'): return { x: b.width - node.width - m.x, y: m.y }
+		case ('bottom-left'):
+		case ('left-bottom'): return { x: m.x, y: b.height - node.height - m.y }
+		case ('bottom-center'):
+		case ('center-bottom'): return { x: b.width / 2 - node.width / 2, y: b.height - node.height - m.y }
+		case ('bottom-right'):
+		case ('right-bottom'): return { x: b.width - node.width - m.x, y: b.height - node.height - m.y }
+		case ('left-center'):
+		case ('center-left'): return { x: m.x, y: b.height / 2 - node.height / 2 }
+		case ('right-center'):
+		case ('center-right'): return { x: b.width - node.width - m.x, y: b.height / 2 - node.height / 2 }
+        default: throw new Error('Invalid placement: ' + placement)
+	}
+}
