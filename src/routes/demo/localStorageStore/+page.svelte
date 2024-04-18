092a8b831c6312932252906b6bea869240c18c14
Braden Wiggins <braden@fractal-hq.com>
2024-04-17 23:35:58 -0400

    [wip] demo localStorageStore page

---
+++ src/routes/demo/localStorageStore/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { localStorageStore } from '$lib'
+	import { onMount } from 'svelte'
+	import { get } from 'svelte/store'
+
+	onMount(() => {
+		localStorage.setItem('test', JSON.stringify("foo"))
+		const store = localStorageStore('test', 'bar')
+		console.log(get(store))
+	})
+</script>
+
+<style lang="scss">
+	:global(.page) {
+		color: red;
+	}
+</style>
