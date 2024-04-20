d5a44a7840d0761950c13bde8d4ed9367faf5bed
Braden Wiggins <braden@fractal-hq.com>
2024-04-19 20:05:23 -0400

    [feat] separate rect and node

--- src/lib/dom/place.ts
+++ src/lib/dom/place.ts
@@ -67,14 +67,14 @@ export function place(
 		options,
 	)
 
-	node =
+	const rect =
 		typeof node === 'string'
 			? select(node)[0]?.getBoundingClientRect()
 			: node instanceof Element
 				? node.getBoundingClientRect()
 				: node
 
-	if (!node) throw new Error('Invalid node: ' + node)
+	if (!rect) throw new Error('Invalid node: ' + node)
 
 	const b =
 		bounds === 'window' && typeof window !== 'undefined'
@@ -82,7 +82,7 @@ export function place(
 			: typeof bounds === 'string'
 				? select(bounds)[0]?.getBoundingClientRect()
 				: bounds ?? { x: 0, y: 0, width: 100, height: 100 }
-	
+
 	if (!b) throw new Error('Invalid bounds: ' + bounds)
 
 	const m = typeof margin === 'number' ? { x: margin, y: margin } : margin
@@ -94,23 +94,23 @@ export function place(
 	// prettier-ignore
 	switch (placement) {
 		case ('center'):
-		case ('center-center'): return { x: b.width / 2 - node.width / 2, y: b.height / 2 - node.height / 2 }
+		case ('center-center'): return { x: b.width / 2 - rect.width / 2, y: b.height / 2 - rect.height / 2 }
 		case ('top-left'):
 		case ('left-top'): return { x: m.x, y: m.y }
 		case ('top-center'):
-		case ('center-top'): return { x: b.width / 2 - node.width / 2, y: m.y }
+		case ('center-top'): return { x: b.width / 2 - rect.width / 2, y: m.y }
 		case ('top-right'):
-		case ('right-top'): return { x: b.width - node.width - m.x, y: m.y }
+		case ('right-top'): return { x: b.width - rect.width - m.x, y: m.y }
 		case ('bottom-left'):
-		case ('left-bottom'): return { x: m.x, y: b.height - node.height - m.y }
+		case ('left-bottom'): return { x: m.x, y: b.height - rect.height - m.y }
 		case ('bottom-center'):
-		case ('center-bottom'): return { x: b.width / 2 - node.width / 2, y: b.height - node.height - m.y }
+		case ('center-bottom'): return { x: b.width / 2 - rect.width / 2, y: b.height - rect.height - m.y }
 		case ('bottom-right'):
-		case ('right-bottom'): return { x: b.width - node.width - m.x, y: b.height - node.height - m.y }
+		case ('right-bottom'): return { x: b.width - rect.width - m.x, y: b.height - rect.height - m.y }
 		case ('left-center'):
-		case ('center-left'): return { x: m.x, y: b.height / 2 - node.height / 2 }
+		case ('center-left'): return { x: m.x, y: b.height / 2 - rect.height / 2 }
 		case ('right-center'):
-		case ('center-right'): return { x: b.width - node.width - m.x, y: b.height / 2 - node.height / 2 }
-        default: throw new Error('Invalid placement: ' + placement)
+		case ('center-right'): return { x: b.width - rect.width - m.x, y: b.height / 2 - rect.height / 2 }
+		default: throw new Error('Invalid placement: ' + placement)
 	}
 }
