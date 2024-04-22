20d242b544fe6ddd25d6af1bee88e0b77a98737b
Braden Wiggins <braden@fractal-hq.com>
2024-04-22 14:31:27 -0400

    [chore] improve naming

--- src/lib/regex/cssVars.ts
+++ src/lib/regex/cssVars.ts
@@ -1,16 +1,15 @@
 /**
  * Regex to extract the inner variable name from a CSS variable.
  * @example
- * | `rgba(var(--var-rgb), 0.5)`.match(CSS_VAR_INNER)[0]
- * > '--var-rgb'
+ * | `rgba(var(--my-color), 0.5)`.match(CSS_VAR_INNER)[0]
+ * > '--my-color'
  */
-// export const CSS_VAR_INNER = /\bvar\(--([a-zA-Z0-9_-]+)\)/g
 export const CSS_VAR_INNER = /\bvar\((--[a-zA-Z0-9_-]+)\)/g
 
 /**
  * Regex to match a CSS variable.
  * @example
- * | `rgba(var(--var-rgb), 0.5)`.match(CSS_VAR)[0]
- * > 'var(--var-rgb)'
+ * | `rgba(var(--my-color), 0.5)`.match(CSS_VAR)[0]
+ * > 'var(--my-color)'
  */
-export const CSS_VAR = /(?:var\()(!?[a-z-]+)/g
+export const CSS_VAR_OUTER = /(?:var\()(!?[a-z-]+)/g
