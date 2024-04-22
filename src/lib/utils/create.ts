ebf541ac2ff3c4775d4ffa2a1cd9aa6e0917beab
Braden Wiggins <braden@fractal-hq.com>
2024-04-21 21:41:06 -0400

    [wip] fractils-inspector

--- src/lib/utils/create.ts
+++ src/lib/utils/create.ts
@@ -2,6 +2,7 @@ import type { TooltipOptions, Tooltip } from '../actions/tooltip'
 import type { JavascriptStyleProperty } from '$lib/css/types'
 
 import { entries } from './object'
+import { DEV } from 'esm-env'
 
 let tooltip: typeof Tooltip
 
@@ -37,7 +38,9 @@ export function create<
 >(
 	tagname: K,
 	options?: TOptions,
-): TOptions extends { tooltip: Partial<TooltipOptions> } ? TElement & { tooltip: Tooltip } : TElement {
+): TOptions extends { tooltip: Partial<TooltipOptions> }
+	? TElement & { tooltip: Tooltip }
+	: TElement {
 	const el = globalThis?.document?.createElement(tagname)
 
 	if (options) {
@@ -94,9 +97,69 @@ export function create<
 		}
 	}
 
+	if (DEV) {
+		const stack = new Error().stack as string
+		maybeAddOpenInEditorEventListener(stack, el)
+	}
+
 	return el as TOptions extends { tooltip: Partial<TooltipOptions> }
 		? TElement & { tooltip: Tooltip }
 		: TElement
+}
+
+/**
+ * Adds an event listener to the element that opens the file in the editor
+ * when the element is right-clicked with `meta` and `alt` keys pressed.
+ *
+ * @todo - This doesn't work yet -- the file/line is inaccurate and doesn't
+ * correspond to the locatioin where the {@link create} function was called.
+ *
+ * @todo - Should this be one event listener that parses the url from the
+ * target node's dataset?
+ *
+ */
+function maybeAddOpenInEditorEventListener(stack: string, el: HTMLElement) {
+	const file = parseFileFromStack(stack)
+	if (file) {
+		el.addEventListener(
+			'contextmenu',
+			e => {
+				if ((e as MouseEvent).metaKey && (e as MouseEvent).altKey) {
+					e.preventDefault()
+					e.stopPropagation()
+					console.log(e.target)
+					openFileInEditor(file)
+				}
+			},
+			{ capture: false },
+		)
+	}
+}
+
+function openFileInEditor(file: string) {
+	const url = '/__open-in-editor?file=' + file
+	fetch(url)
+		.then(response => {
+			console.log(response.status, url)
+		})
+		.catch(error => {
+			console.error('Failed to open file in editor:', error)
+		})
+}
+
+function parseFileFromStack(stack?: string) {
+	const stackLine = stack?.split('\n')[2].trim()
+	const url_regex = /http:\/\/[^ )]+/
+	const timestamp_regex = /\?t=\d+/
+	const url = stackLine?.match(url_regex)?.[0]?.replace(timestamp_regex, '')
+	if (url) {
+		try {
+			const path = new URL(url).pathname.slice(1)
+			return path
+		} catch (e) {
+			console.error('Failed to parse file from stack:', e)
+		}
+	}
 }
 
 // /**
