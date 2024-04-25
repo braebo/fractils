d235e14808650f9357bad27a8e77b873b461159f
Braden Wiggins <braden@fractal-hq.com>
2024-04-25 00:47:00 -0400

    [feat] `styles` class decorator

---
+++ src/lib/decorators/styled.ts
@@ -0,0 +1,34 @@
+import { create } from '../utils/create'
+
+export const styled = createStyledDecorator()
+
+function createStyledDecorator() {
+	return function <T extends { new (...args: any[]): {}; style: string }>(constructor: T) {
+		return class extends constructor {
+			static initialized = false
+			static stylesheet: HTMLStyleElement
+
+			constructor(...args: any[]) {
+				super(...args)
+
+				if (typeof globalThis.document !== 'undefined') {
+					const dis = this.constructor as typeof constructor & {
+						initialized: boolean
+						stylesheet?: HTMLStyleElement
+					}
+
+					if (!dis.style || dis.initialized) {
+						return
+					}
+
+					dis.initialized = true
+
+					dis.stylesheet ??= create('style', {
+						parent: document.head,
+						innerHTML: dis.style,
+					})
+				}
+			}
+		}
+	}
+}
