4e5a7cf43beb67a011032c72e00d754f7c7ba292
Braden Wiggins <braden@fractal-hq.com>
2024-04-26 16:16:57 -0600

    [wip] Rect / resolvers

---
+++ src/lib/dom/rect.ts
@@ -0,0 +1,93 @@
+import type { RectReference, RectTarget, SimpleRect } from './resolveRect'
+
+import { resolveRef, resolveRefRect } from './resolveRect'
+
+export class Rect<T extends RectTarget = RectTarget> {
+	ref: T
+
+	constructor(reference: RectReference) {
+		this.ref = resolveRef(reference) as T
+		const rect = resolveRefRect(this.ref)
+
+		this.#top = () => rect().top
+		this.#right = () => rect().right
+		this.#bottom = () => rect().bottom
+		this.#left = () => rect().left
+		this.#center = () => ({
+			x: this.x + this.width / 2,
+			y: this.y + this.height / 2,
+		})
+	}
+
+	#left: () => number
+	get left() {
+		return this.#left()
+	}
+
+	#right: () => number
+	get right() {
+		return this.#right()
+	}
+
+	#top: () => number
+	get top() {
+		return this.#top()
+	}
+
+	#bottom: () => number
+	get bottom() {
+		return this.#bottom()
+	}
+
+	get x() {
+		return this.#left()
+	}
+
+	get y() {
+		return this.#top()
+	}
+
+	get width() {
+		return this.#right() - this.#left()
+	}
+
+	get height() {
+		return this.#bottom() - this.#top()
+	}
+
+	#center: () => { x: number; y: number }
+	get center() {
+		return this.#center()
+	}
+
+	contains(rect: Rect) {
+		// todo
+	}
+
+	intersects(rect: Rect): { intersects: boolean; deltaX: number; deltaY: number } {
+		let intersects = false
+		let deltaX = 0
+		let deltaY = 0
+
+		// todo
+
+		return { intersects, deltaX, deltaY }
+	}
+}
+
+export function isVirtualRect(reference: RectReference): reference is SimpleRect {
+	return (
+		typeof reference === 'object' &&
+		'x' in reference &&
+		'y' in reference &&
+		'width' in reference &&
+		'height' in reference
+	)
+}
+
+// todo maybe
+export class BoundsRect extends Rect {
+	constructor(reference: RectReference) {
+		super(reference)
+	}
+}
---
+++ src/lib/dom/resolveRect.ts
@@ -0,0 +1,86 @@
+import type { ElementOrSelector } from '../utils/select'
+
+import { select } from '../utils/select'
+import { isVirtualRect } from './rect'
+
+export type RectTarget = DOMRect | HTMLElement | Window
+export type RectReference = SimpleRect | ElementOrSelector | Window | 'window' | 'document'
+export interface SimpleRect {
+	x: number
+	y: number
+	width: number
+	height: number
+}
+
+/**
+ * Resolves a reference to a DOMRect.  Valid input references are:
+ * - {@link ElementOrSelector}
+ * - `{ x, y, width, height }`
+ * - `Window` | `Document`
+ * - `'window'` | `'document'`
+ * - An existing {@link DOMRect}
+ */
+export function resolveRect(ref: RectReference) {
+	return resolveRefRect(resolveRef(ref))
+}
+
+/**
+ * Resolves a reference to an element, window, or DOMRect.
+ * @param ref - A reference to an element, window, or DOMRect.
+ * @returns A function that returns the current DOMRect of the reference.
+ */
+export function resolveRefRect(target: RectTarget) {
+	if ('getBoundingClientRect' in target) {
+		return () => target.getBoundingClientRect()
+	}
+
+	if (target instanceof DOMRect) return () => target
+
+	if (target instanceof Window) {
+		return () => new DOMRect(0, 0, window.innerWidth, window.innerHeight)
+	}
+
+	throw new Error('Invalid target: ' + target)
+}
+
+export function resolveRef(ref: RectReference): RectTarget {
+	if (!ref) {
+		throw new Error('Invalid reference: ' + ref)
+	}
+
+	if (typeof ref === 'string') {
+		if (ref === 'window') {
+			if (!globalThis.window) {
+				throw new Error('Cannot resolve reference to "window": window is undefined.')
+			}
+
+			return globalThis.window as Window
+		}
+
+		if (ref === 'document') {
+			if (!globalThis.document) {
+				throw new Error('Cannot resolve reference to "document": document is undefined.')
+			}
+
+			return globalThis.document.documentElement
+		}
+
+		const element = select(ref)[0]
+
+		if (!element) {
+			throw new Error('Cannot resolve reference to element: element not found.')
+		}
+
+		return element as HTMLElement
+	}
+
+	if (ref instanceof Window) return ref
+	if (ref instanceof HTMLElement) return ref
+	if (ref instanceof DOMRect) return ref
+
+	if (isVirtualRect(ref)) {
+		return new DOMRect(ref.x, ref.y, ref.width, ref.height)
+	}
+
+	throw new Error('Invalid reference: ' + ref)
+}
