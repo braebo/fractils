d4939c75b50eafc99fd044e4b2034a1a7e2bf05d
Braden Wiggins <braden@fractal-hq.com>
2024-04-30 16:12:42 -0400

    [wip] `EventEmitter` class

--- src/lib/utils/EventManager.ts
+++ src/lib/utils/EventManager.ts
@@ -1,13 +1,71 @@
 import { nanoid } from './nanoid'
 
+export type EventCallback = (...args: any[]) => void
+
+/**
+ * A simple event emitter that allows for registering, subscribing to, and emitting events.
+ */
+export class EventEmitter<
+	const TEvents extends readonly string[],
+	const TEventName extends TEvents[number] = TEvents[number],
+> {
+	handlers: Map<TEventName, Map<string, EventCallback>> = new Map()
+	eventNames: TEventName[] = []
+
+	constructor() {}
+
+	registerEvents(events: TEvents) {
+		for (const event of events) {
+			console.log('registering:', event)
+			this.handlers.set(event as TEventName, new Map())
+		}
+	}
+
+	on(event: TEventName, callback: EventCallback): string {
+		const id = nanoid()
+		const listeners = this.handlers.get(event)
+
+		if (!listeners) {
+			console.warn(`Event "${event}" is not registered.`)
+		}
+
+		this.handlers.get(event)?.set(id, callback)
+
+		return id
+	}
+
+	emit(event: TEventName, ...args: Parameters<EventCallback>) {
+		for (const cb of this.handlers.get(event)?.values() ?? []) {
+			cb(...args)
+		}
+	}
+
+	clearAll() {
+		for (const listeners of this.handlers.values()) listeners.clear()
+		this.handlers.clear()
+	}
+}
+
 /**
  * Represents an event manager that provides methods for adding and removing event listeners.
  */
-export class EventManager {
-	listeners = new Map<string, () => void>()
+export class EventManager<
+	const TEvents extends readonly string[] = ['change'],
+	const TEventName extends TEvents[number] = TEvents[number],
+> {
+	listeners = new Map<string, EventCallback>()
+	emitter = new EventEmitter<('change' | TEventName)[]>()
+
+	constructor(events?: TEvents) {
+		if (events) this.register(['change', ...(events as any as TEventName[])])
+	}
+
+	on = this.emitter.on.bind(this.emitter)
+	emit = this.emitter.emit.bind(this.emitter)
+	register = this.emitter.registerEvents.bind(this.emitter)
 
 	/**
-	 * Adds an event listener to an element.
+	 * Adds an event listener to an HTMLElement that will be removed when {@link dispose} is called.
 	 * @param element - The element to add the listener to.
 	 * @param event - The event to listen for.
 	 * @param callback - The callback function to execute when the event is fired.
@@ -26,9 +84,9 @@ export class EventManager {
 		options?: AddEventListenerOptions,
 	) => {
 		const id = nanoid()
-		element.addEventListener(event, callback as EventListener, options)
+		element.addEventListener(event, callback as EventCallback, options)
 		this.listeners.set(id, () => {
-			element.removeEventListener(event, callback as EventListener, options)
+			element.removeEventListener(event, callback as EventCallback, options)
 		})
 		return id
 	}
@@ -44,16 +102,14 @@ export class EventManager {
 	}
 
 	/**
-	 * Removes a specific listener from the event manager
-	 * without removing the listener from the element.
+	 * Removes a listener from the event manager without removing the listener from the element.
 	 */
 	unlisten(id: string): boolean {
 		return this.listeners.delete(id)
 	}
 
 	/**
-	 * Removes all registered listeners from the
-	 * element and clears the event manager.
+	 * Removes all registered listeners from the element and clears the event manager.
 	 */
 	clear(): void {
 		for (const cb of this.listeners.values()) cb()
@@ -65,5 +121,10 @@ export class EventManager {
 	 */
 	dispose(): void {
 		this.clear()
+		this.emitter.clearAll()
 	}
 }
+
+const test2 = new EventManager(['click', 'foo'])
+test2.on('change', () => {})
+// test2.on('change', () => {})
