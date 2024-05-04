6cba359cf02da2c4b6dcb2d51ab38491e6159856
Braden Wiggins <braden@fractal-hq.com>
2024-05-04 00:33:44 -0400

    [feat] unit testing env

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,16 @@ export default defineConfig({
 		},
 	},
 	define: {
+		// Determines the maximum log level for `Logger` globally.
 		'import.meta.env.FRACTILS_LOG_LEVEL': JSON.stringify('info'),
+		// Disables `Logger` during tests when empty ('').
+		'import.meta.env.FRACTILS_LOG_VITEST': JSON.stringify(''),
 	},
 	test: {
-		environment: 'happy-dom',
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: './mocks.ts',
+		include: ['**/src/lib/**/*.test.ts'],
+		exclude: ['test/**.*', '**/scripts/**.*'],
 	},
 })
