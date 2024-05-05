7eccdf496a31a9ea79c3f61823a35673c0340d40
Braden Wiggins <braden@fractal-hq.com>
2024-05-04 21:21:40 -0400

    [feat] update tests for new deepMerge

--- src/lib/utils/deepMerge.test.ts
+++ src/lib/utils/deepMerge.test.ts
@@ -6,21 +6,21 @@ describe('deepMerge', () => {
 		const a = { 0: 1 }
 		const b = { 1: 1 }
 		const x = { 0: 1, 1: 1 }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 
 	it('should merge nested objects', () => {
 		const a = { 0: { 0: 0 } }
 		const b = { 0: { 1: 0 } }
 		const x = { 0: { 0: 0, 1: 0 } }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 
 	it('should not mutate the original objects', () => {
 		const a = {}
 		const b = { 0: 1 }
 		const x = {}
-		deepMerge(a, b)
+		deepMerge([a, b])
 		expect(a).toEqual(x)
 	})
 
@@ -28,62 +28,62 @@ describe('deepMerge', () => {
 		const a = { 0: undefined, 1: undefined, 2: undefined, 3: undefined }
 		const b = { 0: 1, 1: '2', 2: true, 3: false }
 		const x = { 0: 1, 1: '2', 2: true, 3: false }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 
 	it('should accept `0`', () => {
 		const a = { 0: 1 }
 		const b = { 0: 0 }
 		const x = { 0: 0 }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 
 	it('should reject `true`', () => {
 		const a = { 0: { 0: 0 } }
 		const b = { 0: true }
 		const x = { 0: { 0: 0 } }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 
 	it('should accept `false`', () => {
 		const a = { 0: { 0: 0 } }
 		const b = { 0: false }
 		const x = { 0: false }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 
 	it('should reject `undefined`', () => {
 		const a = { 0: 0 }
 		const b = { 0: undefined }
 		const x = { 0: 0 }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 
 	it('should merge `true`, but not `false`', () => {
 		const a = { 0: true, 1: 1 }
 		const b = { 0: 1, 1: false }
 		const x = { 0: 1, 1: false }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 
 	it('should concatenate arrays', () => {
 		const a = { 0: [0] }
 		const b = { 0: [1] }
 		const x = { 0: [0, 1] }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 
 	it('should de-duplicate arrays', () => {
 		const a = { 0: [1, 2] }
 		const b = { 0: [2, 3] }
 		const x = { 0: [1, 2, 3] }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 
 	it('should accept strings', () => {
 		const a = { 0: { 0: 0 } }
 		const b = { 0: '1' }
 		const x = { 0: '1' }
-		expect(deepMerge(a, b)).toEqual(x)
+		expect(deepMerge([a, b])).toEqual(x)
 	})
 })
