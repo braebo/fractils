24c2ee0624f57daf66f001ba9948350d6a9c6e5d
Braden Wiggins <braden@fractal-hq.com>
2024-05-09 21:13:40 -0400

    feat: logger refactor

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -1,12 +1,63 @@
-import { randomColor, type CSSColor } from '../color/css'
+import { randomCSSColorName, type CSSColorName } from '../color/css'
 
-import { BROWSER } from '../dom/BROWSER'
 import { stringify } from './stringify'
 import { b, r, y, gr, dim } from './l'
+import { DEV, BROWSER } from './env'
 import { isSafari } from './safari'
-import { defer } from './defer'
-import { DEV } from 'esm-env'
 import { debrief } from './debrief'
+import { defer } from './defer'
+
+export interface LoggerOptions {
+	/**
+	 * Whether to use the styled logger or the regular console.log.
+	 * @defaultValue true
+	 */
+	styled?: boolean
+	/**
+	 * Whether to defer the log to the next idle state.  Disabled on Safari to avoid crashing.
+	 * @defaultValue true
+	 */
+	deferred?: boolean
+	/**
+	 * The foreground color of the log.
+	 * @defaultValue randomColor()
+	 */
+	fg?: CSSColorName | (string & {})
+	/**
+	 * The background color of the log.
+	 * @defaultValue transparent
+	 */
+	bg?: CSSColorName | (string & {})
+	/**
+	 * Any additional CSS to apply to the log.
+	 * @defaultValue ''
+	 */
+	css?: string
+	/**
+	 * Run the logger on the server.
+	 * @defaultValue false
+	 */
+	server?: boolean
+	/**
+	 * Run the logger in the browser.
+	 * @defaultValue true
+	 */
+	browser?: boolean
+	/**
+	 * Whether to only run the logger in development mode.
+	 * @defaultValue true
+	 */
+	devOnly?: boolean
+	/**
+	 * Print's the url of the file that called the logger.
+	 */
+	callsite?: boolean
+	/**
+	 * The title of the logger.  Prepended to all logs.
+	 * @defaultValue ''
+	 */
+	title?: string
+}
 
 // todo - Is there a reliable way to type an ImportMetaEnv entry globally for consumers?
 const ENABLED =
@@ -19,13 +70,22 @@ const bypassDefer = false
 export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal' | 'off'
 
 export class Logger {
-	#logger: ReturnType<typeof logger>
+	title = ''
+	options: LoggerOptions
 
-	constructor(
-		public title: string,
-		public options?: Parameters<typeof logger>[1],
-	) {
-		this.#logger = logger(title, options)
+	#logger: (...args: any[]) => void
+
+	constructor(title: string, options?: LoggerOptions)
+	constructor(options: LoggerOptions)
+	constructor(titleOrOptions: string | LoggerOptions, options?: LoggerOptions) {
+		if (typeof titleOrOptions === 'string') {
+			this.title = titleOrOptions
+			this.options = options ?? {}
+		} else {
+			this.options = titleOrOptions
+			this.title = titleOrOptions.title ?? ''
+		}
+		this.#logger = Logger.createLogger(this.title, this.options)
 		return this
 	}
 
@@ -38,36 +98,48 @@ export class Logger {
 	 * @param args
 	 */
 	log = (...args: any[]) => {
+		this.#logger(...args)
+	}
+
+	/**
+	 * Logs any args as well as any logs in the current buffer.
+	 * @param args
+	 */
+	dump = (...args: any[]) => {
 		if (this.buffer.length) {
+			if (args[0].match(/ⓘ|⚠|⛔|💀/)) {
+				this.buffer.unshift(args.shift())
+			}
 			this.#logger(...this.buffer, ...args)
-			this.buffer = []
 		} else {
 			this.#logger(...args)
 		}
+
+		this.buffer = []
 	}
 
 	debug(...args: any[]) {
-		if (import.meta.env.FRACTILS_LOG_LEVEL === 'debug') this.log('🐞', ...args)
+		if (import.meta.env.FRACTILS_LOG_LEVEL === 'debug') this.dump('🐞', ...args)
 		return this
 	}
 
 	info(...args: any[]) {
-		this.log(b('ⓘ'), ...args)
+		this.dump(b('\nⓘ'), ...args)
 		return this
 	}
 
 	warn(...args: any[]) {
-		this.log(y('⚠'), ...args)
+		this.dump(y('\n⚠'), ...args)
 		return this
 	}
 
 	error(...args: any[]) {
-		this.log(r('⛔'), ...args)
+		this.dump(r('\n⛔'), ...args)
 		return this
 	}
 
 	fatal(...args: any[]) {
-		this.log(r('💀'), ...args)
+		this.dump(r('\n💀'), ...args)
 		return this
 	}
 
@@ -110,15 +182,10 @@ export class Logger {
 	 *
 	 * @example
 	 * ```typescript
-	 * class MyClass {
-	 * 	#log = new Logger('MyClass', { fg: 'cyan' })
-	 *
-	 * 	foo(a: number) {
-	 * 		this.#log.fn('foo', a).info()
-	 * 	}
-	 * }
-	 *
-	 * new MyClass().foo(1) // MyClass ⓘ   foo(1)
+	 * const log = new Logger('Foo')
+	 * const bar = (a: number) => log.fn('bar', a)
+	 * bar(1) // logs:
+	 * ⓘ Foo bar(1)
 	 * ```
 	 */
 	fn(str: string, ...args: any[]) {
@@ -130,191 +197,80 @@ export class Logger {
 		)
 		return this
 	}
-}
 
-export const logger = (
-	title = 'LOG',
-	options?: {
-		// todo - Add a log level override option.
-		/**
-		 * Whether to use the styled logger or the regular console.log.
-		 * @defaultValue true
-		 */
-		styled?: boolean
-		/**
-		 * Whether to defer the log to the next idle state.  Disabled on Safari to avoid crashing.
-		 * @defaultValue true
-		 */
-		deferred?: boolean
-		/**
-		 * The foreground color of the log.
-		 * @defaultValue randomColor()
-		 */
-		fg?: CSSColor | (string & {})
-		/**
-		 * The background color of the log.
-		 * @defaultValue transparent
-		 */
-		bg?: CSSColor | (string & {})
-		/**
-		 * Any additional CSS to apply to the log.
-		 * @defaultValue ''
-		 */
-		css?: string
-		/**
-		 * Run the logger on the server.
-		 * @defaultValue false
-		 */
-		server?: boolean
-		/**
-		 * Run the logger in the browser.
-		 * @defaultValue true
-		 */
-		browser?: boolean
-		/**
-		 * Whether to only run the logger in development mode.
-		 * @defaultValue true
-		 */
-		devOnly?: boolean
-		/**
-		 * Print's the url of the file that called the logger.
-		 */
-		callsite?: boolean
-	},
-) => {
-	options ??= {}
+	static createLogger(title: string, options?: LoggerOptions) {
+		options ??= {}
 
-	const fg = options.fg || randomColor()
-	const bg = options.bg || 'transparent'
-	const css = options.css ?? ''
-	const browser = options.browser ?? true
-	const server = options.server ?? false
+		const browser = options.browser ?? true
+		const server = options.server ?? false
 
-	if (BROWSER() && !browser) return () => void 0
-	if (!BROWSER() && !server) return () => void 0
+		if (BROWSER && !browser) return () => void 0
+		if (!BROWSER && !server) return () => void 0
 
-	options.styled ??= true
-	const styled = options.styled && !bypassStyles
+		const fg = options.fg || randomCSSColorName()
+		const bg = options.bg || 'transparent'
+		const css = options.css ?? ''
 
-	options.deferred ??= true
-	const deferred = options.deferred && !bypassDefer && !isSafari()
+		options.styled ??= true
+		const styled = options.styled && !bypassStyles
 
-	if (!ENABLED) return () => void 0
+		options.deferred ??= true
+		const deferred = options.deferred && !bypassDefer && !isSafari()
 
-	let callsite: URL | undefined = undefined
-	if (options.callsite || true) {
-		callsite = getCallSite().url
-	}
+		if (!ENABLED) return () => void 0
 
-	const fn = !styled
-		? (...args: any[]) => {
-				console.log(`| ${callsite} |\n| ${title} |`, ...args)
-			}
-		: (...args: any[]) => {
-				let messageConfig = '%c%s%c '
+		let callsite: URL | undefined = undefined
 
-				args.forEach(argument => {
-					const type = typeof argument
-					switch (type) {
-						case 'bigint':
-						case 'number':
-							messageConfig += '%d   '
-							break
+		const log = !styled
+			? (...args: any[]) => {
+					console.log(`| ${callsite} |\n| ${title} |`, ...args)
+				}
+			: (...args: any[]) => {
+					let messageConfig = '%c%s%c '
 
-						case 'string':
-							messageConfig += '%s   '
-							break
+					args.forEach(argument => {
+						const type = typeof argument
+						switch (type) {
+							case 'bigint':
+							case 'number':
+								messageConfig += '%d   '
+								break
 
-						case 'object':
-						case 'boolean':
-						case 'undefined':
-						default:
-							messageConfig += '%o   '
-					}
-				})
+							case 'string':
+								messageConfig += '%s   '
+								break
 
-				messageConfig += '%c%s'
+							case 'object':
+							case 'boolean':
+							case 'undefined':
+							default:
+								messageConfig += '%o   '
+						}
+					})
 
-				console.log(
-					messageConfig,
-					`color:${fg};background:${bg};padding:0.1rem;${css}`,
-					// `${title.padEnd(10, ' ')} |`,
-					`${title.padEnd(10, ' ')}`,
-					`color:initial;background:${bg};padding:0.1rem;${css}`,
-					...args.map(
-						a =>
+					messageConfig += '%c%s'
+
+					console.log(
+						messageConfig,
+						`color:${fg};background:${bg};padding:0.1rem;${css}`,
+						// `${title.padEnd(10, ' ')} |`,
+						`${title.padEnd(10, ' ')}`,
+						`color:initial;background:${bg};padding:0.1rem;${css}`,
+						...args.map(a =>
 							// Testing console goes nuts with large objects, so we debrief them.
 							import.meta.env.VITEST ? debrief(a, { depth: 1, siblings: 1 }) : a,
-						// a
-					),
-					`color:#666;background:${bg};padding:0.1rem;${css};font-size:0.66rem;`,
-					options?.callsite ? `${callsite}` : '',
-				)
-			}
+						),
+						`color:#666;background:${bg};padding:0.1rem;${css};font-size:0.66rem;`,
+						options?.callsite ? `${callsite}` : '',
+					)
+				}
 
-	if (!deferred) return fn
+		if (!deferred) return log
 
-	return (...args: any[]) => defer(() => fn(...args))
+		return (...args: any[]) => defer(() => log(...args))
+	}
 }
 
-/**
- * Gets the call site of a logger function.
- * @returns A url to the file, the file name, line number, and column number of the call site.
- */
-function getCallSite() {
-	if (import.meta.env.VITEST) return failed(true)
-	const err = new Error()
-	const stackLines = err.stack?.split('\n').slice(2).filter(Boolean)
-
-	let match: string | undefined
-	let matches = [] as (string | undefined)[]
-
-	// Everything
-	while (!match && stackLines?.length) {
-		match = matchLine(stackLines.shift())
-	}
-
-	if (!match) return failed()
-
-	try {
-		const url = new URL(match || import.meta.url)
-		const [_t, lineNumber, columnNumber] = url.searchParams.get('t')?.split(':') || []
-		const callsite = `${url.pathname}:${lineNumber}:${columnNumber}`
-		const filename = url.pathname.split('/').pop()
-
-		return {
-			callsite,
-			filename,
-			url,
-		}
-	} catch (error) {
-		return failed()
-	}
-
-	function matchLine(line: any) {
-		if (!line) return ''
-		if (line.includes('logger.ts')) return ''
-
-		const regex = /https?:\/\/[^\s)]+(?=\)?)/g //? advanced
-		const match = line.match(regex)?.[0]
-		return match || ''
-	}
-
-	function failed(silent = DEV && BROWSER()) {
-		if (!silent) {
-			console.warn('getCallSite(): Failed to parse call site from stack trace.')
-
-			console.groupCollapsed('getCallSite(): debug info')
-			console.log('match:', match)
-			console.log('match attempts:', matches)
-			console.log('stackLines:', stackLines)
-			console.log('err:', err)
-			console.groupEnd()
-		}
-		return {
-			callsite: '/unknown:0:0',
-			filename: 'unknown',
-			url: new URL('about:blank'),
-		}
-	}
+export const logger = (title = 'LOG', options?: LoggerOptions) => {
+	return Logger.createLogger(title, options)
 }
