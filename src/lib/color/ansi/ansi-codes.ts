3704c4afead5cf39161ff11235892a30022404cb
Braden Wiggins <braden@fractal-hq.com>
2024-05-09 21:14:05 -0400

    wip: ansi colors / color refactor

---
+++ src/lib/color/ansi/ansi-codes.ts
@@ -0,0 +1,106 @@
+import { hexToRgb, rgbToAnsi256, rgbToAnsi16, ansi256To16 } from './utils'
+import { getColorSpace } from './color-support'
+
+type StyleObject = {
+	[key: string]: string
+	open: string
+	close: string
+}
+
+const colorSpace = getColorSpace()
+export const isSupported = colorSpace > 0
+const mono: StyleObject = { open: '', close: '' }
+const monoFn = () => mono
+const esc = isSupported
+	? (open: number | string, close: number | string) => ({
+			open: `\x1b[${open}m`,
+			close: `\x1b[${close}m`,
+		})
+	: monoFn
+const closeCode = 39
+const bgCloseCode = 49
+
+// defaults, true color
+let fnAnsi256 = (code: number) => esc(`38;5;${code}`, closeCode)
+let fnBgAnsi256 = (code: number) => esc(`48;5;${code}`, bgCloseCode)
+let fnRgb = (r: number, g: number, b: number) => esc(`38;2;${r};${g};${b}`, closeCode)
+let fnBgRgb = (r: number, g: number, b: number) => esc(`48;2;${r};${g};${b}`, bgCloseCode)
+
+const createRgbFn = (fn: (code: number) => StyleObject) => (r: number, g: number, b: number) =>
+	fn(rgbToAnsi256(r, g, b))
+
+const createHexFn = (fn: (r: number, g: number, b: number) => StyleObject) => (hex: string) => {
+	let [r, g, b] = hexToRgb(hex)
+	return fn(r, g, b)
+}
+
+if (colorSpace === 1) {
+	// ANSI 16 colors
+	fnAnsi256 = code => esc(ansi256To16(code), closeCode)
+	fnBgAnsi256 = code => esc(ansi256To16(code) + 10, bgCloseCode)
+	fnRgb = (r, g, b) => esc(rgbToAnsi16(r, g, b), closeCode)
+	fnBgRgb = (r, g, b) => esc(rgbToAnsi16(r, g, b) + 10, bgCloseCode)
+} else if (colorSpace === 2) {
+	// ANSI 256 colors
+	fnRgb = createRgbFn(fnAnsi256)
+	fnBgRgb = createRgbFn(fnBgAnsi256)
+}
+
+export const baseStyles: Record<string, StyleObject> = {
+	// misc
+	visible: mono,
+	reset: esc(0, 0),
+	inverse: esc(7, 27),
+	hidden: esc(8, 28),
+
+	// styles
+	bold: esc(1, 22),
+	dim: esc(2, 22),
+	italic: esc(3, 23),
+	underline: esc(4, 24),
+	strikethrough: esc(9, 29),
+	strike: esc(9, 29), // alias for strikethrough
+
+	grey: esc(90, closeCode), // UK spelling alias for blackBright
+	gray: esc(90, closeCode), // US spelling alias for blackBright
+	bgGrey: esc(100, bgCloseCode), // UK spelling alias for bgBlackBright
+	bgGray: esc(100, bgCloseCode), // US spelling alias for bgBlackBright
+}
+
+// generate ANSI 16 colors dynamically and save ~450 bytes
+let styles = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+let bright = 'Bright'
+let code = 30
+let name: string
+let bgName: string
+
+for (name of styles) {
+	bgName = 'bg' + name[0].toUpperCase() + name.slice(1)
+
+	baseStyles[name] = esc(code, closeCode) // Element implicitly has an 'any' type because expression of type 'string' can't be used to index type '{ visible: { open: string; close: string; }; reset: { open: string; close: string; }; inverse: { open: string; close: string; }; hidden: { open: string; close: string; }; bold: { open: string; close: string; }; dim: { ...; }; ... 7 more ...; bgGray: { ...; }; }'.  // No index signature with a parameter of type 'string' was found on type '{ visible: { open: string; close: string; }; reset: { open: string; close: string; }; inverse: { open: string; close: string; }; hidden: { open: string; close: string; }; bold: { open: string; close: string; }; dim: { ...; }; ... 7 more ...; bgGray: { ...; }; }'.ts(7053)
+	baseStyles[name + bright] = esc(code + 60, closeCode) // Element implicitly has an 'any' type because expression of type 'string' can't be used to index type '{ visible: { open: string; close: string; }; reset: { open: string; close: string; }; inverse: { open: string; close: string; }; hidden: { open: string; close: string; }; bold: { open: string; close: string; }; dim: { ...; }; ... 7 more ...; bgGray: { ...; }; }'.  // No index signature with a parameter of type 'string' was found on type '{ visible: { open: string; close: string; }; reset: { open: string; close: string; }; inverse: { open: string; close: string; }; hidden: { open: string; close: string; }; bold: { open: string; close: string; }; dim: { ...; }; ... 7 more ...; bgGray: { ...; }; }'.ts(7053)
+	baseStyles[bgName] = esc(code + 10, bgCloseCode) // etc
+	baseStyles[bgName + bright] = esc(code + 70, bgCloseCode) // etc
+
+	code++
+}
+
+export const styleMethods = {
+	fg: fnAnsi256,
+	bg: fnBgAnsi256,
+	rgb: fnRgb,
+	bgRgb: fnBgRgb,
+	// note: the `...` operator is too slow
+	//hex: (hex) => fnRgb(...hexToRgb(hex)),
+	hex: createHexFn(fnRgb),
+	// note: the `...` operator is too slow
+	//bgHex: (hex) => fnBgRgb(...hexToRgb(hex)),
+	bgHex: createHexFn(fnBgRgb),
+
+	// reserved for future: hyperlink (OSC 8) is not widely supported (works in iTerm)
+	// link: hasColor
+	//   ? (url) => ({ open: ESC + ']8;;' + url + BEL, close: ESC + ']8;;' + BEL })
+	//   : (url) => ({ open: '', close: `(${ZWSP}${url}${ZWSP})` }),
+}
+
+export const rgb = fnRgb
---
+++ src/lib/color/ansi/utils.ts
@@ -0,0 +1,106 @@
+const { round, floor, max } = Math
+
+/**
+ * Replace all matched strings.
+ * Note: this implementation is over 30% faster than String.replaceAll().
+ */
+export const replaceAll = (str: string, searchValue: string, replaceValue: string): string => {
+	// visible style has empty open/close props
+	if (searchValue === '') return str
+
+	let substringLength = searchValue.length
+	let lastPos = 0
+	let result = ''
+	let pos
+
+	while (~(pos = str.indexOf(searchValue, lastPos))) {
+		result += str.slice(lastPos, pos) + replaceValue
+		lastPos = pos + substringLength
+	}
+
+	return lastPos ? result + str.slice(lastPos) : str
+}
+
+/**
+ * Convert hex color string to RGB values.
+ *
+ * A hexadecimal color code can be 3 or 6 digits with an optional "#" prefix.
+ *
+ * The 3 digits specifies an RGB doublet data as a fully opaque color.
+ * For example, "#123" specifies the color that is represented by "#112233".
+ *
+ * The 6 digits specifies a fully opaque color.
+ * For example, "#112233".
+ *
+ * @param value A string that contains the hexadecimal RGB color representation.
+ * @returns The red, green, blue values in range [0, 255].
+ */
+export const hexToRgb = (value: string): [number, number, number] => {
+	let [, color] = /([a-f\d]{3,6})/i.exec(value) || []
+	let len = color ? color.length : 0
+
+	if (len === 3) {
+		color = color[0] + color[0] + color[1] + color[1] + color[2] + color[2]
+	} else if (len !== 6) {
+		return [0, 0, 0]
+	}
+
+	let num = parseInt(color, 16)
+
+	return [(num >> 16) & 255, (num >> 8) & 255, num & 255]
+}
+
+/**
+ * Convert RGB values to approximate code of ANSI 256 colors.
+ *
+ * Original source: https://github.com/Qix-/color-convert/blob/master/conversions.js#L551
+ */
+export const rgbToAnsi256 = (r: number, g: number, b: number): number => {
+	// greyscale
+	if (r === g && g === b) {
+		if (r < 8) return 16
+		if (r > 248) return 231
+		return round(((r - 8) / 247) * 24) + 232
+	}
+
+	return (
+		16 +
+		// r / 255 * 5 => r / 51
+		36 * round(r / 51) +
+		6 * round(g / 51) +
+		round(b / 51)
+	)
+}
+
+/**
+ * Convert ANSI 256 color code to approximate code of ANSI 16 colors.
+ */
+export const ansi256To16 = (code: number): number => {
+	let r, g, b
+
+	if (code < 8) return 30 + code
+	if (code < 16) return 90 + (code - 8)
+
+	if (code >= 232) {
+		// greyscale
+		r = g = b = ((code - 232) * 10 + 8) / 255
+	} else {
+		code -= 16
+
+		const remainder = code % 36
+
+		r = floor(code / 36) / 5
+		g = floor(remainder / 6) / 5
+		b = (remainder % 6) / 5
+	}
+
+	const value = max(r, g, b) * 2
+
+	if (value === 0) return 30
+
+	let code16 = 30 + ((round(b) << 2) | (round(g) << 1) | round(r))
+
+	return value === 2 ? code16 + 60 : code16
+}
+
+export const rgbToAnsi16 = (r: number, g: number, b: number) => ansi256To16(rgbToAnsi256(r, g, b))
