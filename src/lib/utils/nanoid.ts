02937d301bb92e6dc86ad1782f2c2c5065326d02
Braden Wiggins <braden@fractal-hq.com>
2024-05-13 15:01:23 -0400

    feat: document `nanoid`

--- src/lib/utils/nanoid.ts
+++ src/lib/utils/nanoid.ts
@@ -1,6 +1,10 @@
-export function nanoid(t = 21) {
+/**
+ * Generate a random ID.
+ * @param length The length of the ID to generate.  Default: `21`
+ */
+export function nanoid(length = 21) {
 	return crypto
-		.getRandomValues(new Uint8Array(t))
+		.getRandomValues(new Uint8Array(length))
 		.reduce(
 			(t, e) =>
 				(t +=
