73b9e443ee8cddb4ba00268cd5f2cf71a1707082
Braden Wiggins <braden@fractal-hq.com>
2024-05-14 15:06:31 -0400

    feat: new animated icons

---
+++ src/lib/gui/svg/RenameSVG.ts
@@ -0,0 +1,103 @@
+import { styled } from '../../decorators/styled'
+import { Tooltip } from '../../actions/tooltip'
+import { create } from '../../utils/create'
+
+@styled
+export class RenameSVG {
+	class = 'fracgui-icon-rename'
+	element: HTMLElement & { tooltip: Tooltip }
+	classes = [this.class, 'fracgui-icon']
+
+	constructor() {
+		this.element = create('div', {
+			classes: this.classes,
+			innerHTML: /*html*/ `
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" >
+                    <path class="cursor top" fill="none" stroke="currentColor" d="M5 4h1a3 3 0 0 1 3 3 3 3 0 0 1 3-3h1" vector-effect="non-scaling-stroke"></path>
+                    <path class="cursor bottom"fill="none" stroke="currentColor" d="M13 20h-1a3 3 0 0 1-3-3 3 3 0 0 1-3 3H5" vector-effect="non-scaling-stroke"></path>
+                    <rect class="box full" fill="currentColor" stroke="none" x="1" y="8" width="18" height="8" rx="2" vector-effect="non-scaling-stroke"></rect>
+                    <path class="cursor middle"fill="none" stroke="currentColor" d="M9 7v10" vector-effect="non-scaling-stroke"></path>
+                </svg>
+            `.replaceAll(/\t|\n/g, ''),
+		}) as HTMLElement & { tooltip: Tooltip }
+	}
+
+	static style = /*css*/ `
+        .fracgui-icon-rename {
+            width: 1.5rem;
+            height: 1.5rem;
+
+            color: var(--fracgui-controller-dim_color);
+            margin-right: 0.5rem;
+            padding: 0.125rem;
+
+            z-index: 1;
+            cursor: pointer;
+
+            pointer-events: all;
+        }
+
+        .fracgui-icon-rename.disabled {
+            /* pointer-events: none; */
+            opacity: 0.5;
+            cursor: default;
+        }
+
+        .fracgui-icon-rename svg {
+            width: 100%;
+            height: 100%;
+            
+            pointer-events: none;
+        }
+
+        .fracgui-icon-rename path {
+            color: var(--fracgui-controller-dim_color);
+            /* stroke: var(--fracgui-controller-dim_color); */
+        }
+        .fracgui-icon-rename .cursor {
+            transform: translate(2px, 0) scale(1, 0.9);
+            
+            transition: 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+            transition-delay: 0s;
+            transition-property: opacity, transform;
+            transform-origin: center;
+        }
+        .fracgui-icon-rename:hover .cursor, .fracgui-icon-rename.active .cursor {
+            transform: translate(0, 0);
+            transform: translate(0, 0) scale(1, 0.9);
+        }
+        .fracgui-icon-rename.active .cursor {
+            color: var(--fracgui-theme-a);
+            animation: blink 1.5s infinite;
+            animation-delay: 0.5s;
+        }
+
+        @keyframes blink {
+            0%, 45%, 80% {
+                opacity: 1;
+            }
+            50%, 75% {
+                opacity: 0;
+            }
+        }
+
+        .fracgui-icon-rename .box {
+            color: var(--fracgui-folder-dim_color);
+            
+            opacity: 0;
+            transform: scale(0);
+            
+            transition: 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+            transition-property: opacity, transform;
+            transform-origin: center;
+        }
+        .fracgui-icon-rename:hover .box, .fracgui-icon-rename.active .box {
+            opacity: 0.25;
+            transform: scale(1.1);
+            color: var(--fracgui-controller_color);
+        }
+        .fracgui-icon-rename:hover .box {
+            transform: scale(1);
+        }
+    `
+}
---
+++ src/lib/gui/svg/SaveSVG.ts
@@ -0,0 +1,92 @@
+import type { Tooltip } from '../../actions/tooltip'
+
+import { styled } from '../../decorators/styled'
+import { create } from '../../utils/create'
+
+@styled
+export class SaveSVG {
+	class = 'fracgui-icon-save'
+	element: HTMLElement & { tooltip: Tooltip }
+	classes = [this.class, 'fracgui-icon']
+
+	constructor() {
+		this.element = create('div', {
+			classes: this.classes,
+			innerHTML: /*html*/ `
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <g class="Frame">
+                        <!-- <g class="back-group"> -->
+                        <path class="back-group" fill="currentColor" fill-rule="evenodd" d="M6.127 21A3.496 3.496 0 0 0 9 22.5h10a3.5 3.5 0 0 0 3.5-3.5V9c0-1.19-.593-2.24-1.5-2.873V7.5c.314.418.5.937.5 1.5v10a2.5 2.5 0 0 1-2.5 2.5H9a2.489 2.489 0 0 1-1.5-.5H6.127Z" class="Subtract" clip-rule="evenodd"/>
+                        <!-- </g> -->
+                        <rect width="16" height="16" x="3" y="3" fill="currentColor" stroke-width="1.5" class="front" rx="2"/>
+                        <g class="plus">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M11 8v7" class="vertical"/>
+                            <path stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M14.5 11.5h-7" class="horizontal"/>
+                        </g>
+                    </g>
+                </svg>
+            `.replaceAll(/\t|\n/g, ''),
+			tooltip: {
+				text: 'New Preset',
+				delay: 1000,
+				placement: 'left',
+			},
+		})
+	}
+
+	static style = /*css*/ `
+        .fracgui-icon-save {
+            width: 1.5rem;
+            height: 1.5rem;
+
+            color: var(--fracgui-controller-dim_color);;
+            margin-right: 0.25rem;
+
+            z-index: 1;
+            cursor: pointer;
+        }
+
+        .fracgui-icon-save .back-group {
+            transition: 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+            transition-property: opacity, transform;
+            transition-delay: 0s;
+            
+            opacity: 0;
+            transform: translate(-0.25rem, -0.25rem);
+        }
+        .fracgui-icon-save:hover .back-group {
+            opacity: 1;
+            transition-delay: 0.25s;
+            transform: translate(0, 0);
+        }
+
+        .fracgui-icon-save .plus {
+            transition: 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+            transition-property: color, transform;
+            transform-origin: center;
+
+            transform: scale(1.5);
+        }
+        .fracgui-icon-save:hover .plus {
+            transform: scale(1);
+            color: var(--fracgui-bg-a);
+        }
+
+        .fracgui-icon-save .front {
+            transition: 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+            transition-property: transform, opacity;
+
+            transform: scale(1.25);
+            opacity: 0;
+        }
+        .fracgui-icon-save:hover .front {
+            transform: scale(1);
+            opacity: 1;
+        }
+
+        .fracgui-icon-save svg {
+            width: 100%;
+            height: 100%;
+        }
+    `
+}
