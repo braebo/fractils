a267e80765101fe34bd876505c86c8b734dfd734
Braden Wiggins <braden@fractal-hq.com>
2024-05-16 21:26:42 -0400

    chore: remove resiable test

--- src/routes/demo/docinator/+page.svelte
+++ src/routes/demo/docinator/+page.svelte
@@ -4,16 +4,9 @@
 	import type { PageData } from './$types'
 
 	import Doc from '$examples/_lib/Doc/Doc.svelte'
-	import Resizable from '$lib/components/Resizable.svelte'
 
 	export let data: PageData
 </script>
-
-<Resizable>
-	<h1 class="col">wow</h1>
-</Resizable>
-
-<Resizable --width="20rem" />
 
 {#each data.docs as contents}
 	{#if contents}
