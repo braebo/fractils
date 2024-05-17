97a48f9a6113285760cb26a71eadca01269fc80c
Braden Wiggins <braden@fractal-hq.com>
2024-05-16 23:41:28 -0400

    feat: add `get` and `set` methods / add example

--- src/lib/utils/persist.ts
+++ src/lib/utils/persist.ts
@@ -1,16 +1,33 @@
 import { DEV } from 'esm-env'
 
 /**
- * Persists a value in localStorage, and updates it whenever the `value` property is reassigned.
- * @returns An object with a `value` property that syncs with localStorage.
+ * Persists a value in localStorage.
+ * @returns An object with a `get` and `set` method that syncs with localStorage.
+ *
+ * @example
+ * ```ts
+ * // Let's say 'foo' is already "42" in localStorage:
+ *
+ * const foo = persist('foo', 5) // initial value won't be used
+ *
+ * foo.value // 42
+ * // or
+ * foo.get() // 42
+ *
+ * foo.value = 123
+ * // or
+ * foo.set(123)
+ *
+ * localStorage.getItem('foo') // "123"
+ * ```
  */
 export function persist<T>(
 	/**
-	 * The key to store the value under in localStorage.
+	 * The key to store the value under in local storage.
 	 */
 	key: string,
 	/**
-	 * The initial value to store (optional).
+	 * The initial value to store if the key is'nt found in local storage.
 	 * @default undefined
 	 */
 	initialValue: T | undefined = undefined,
@@ -22,20 +39,26 @@ export function persist<T>(
 ) {
 	const bail = () => {
 		if (typeof localStorage === 'undefined') {
-			if (!quiet) console.warn('localStorage is not available for key', key)
+			if (!quiet) console.warn(`localStorage is not available for key "${key}"`)
 			return true
 		} else return false
 	}
 
 	return {
-		get value(): T {
+		get(): T {
 			if (bail()) return initialValue!
 			const storedValue = localStorage.getItem(key)
 			return storedValue ? JSON.parse(storedValue) : initialValue
 		},
-		set value(newValue) {
+		set(newValue: T) {
 			if (bail()) return
 			localStorage.setItem(key, JSON.stringify(newValue))
+		},
+		get value() {
+			return this.get()
+		},
+		set value(v) {
+			this.set(v)
 		},
 	}
 }
