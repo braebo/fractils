1960721bc2362c9c814801ba366ef1f49df1f8c5
Braden Wiggins <braden@fractal-hq.com>
2024-05-20 14:05:32 -0400

    fix: comment out test stuff

--- src/lib/utils/isType.ts
+++ src/lib/utils/isType.ts
@@ -16,79 +16,79 @@ export function isType<const _T extends any, const U extends string>(
 	return '__type' in value && (value as any)['__type'] === type
 }
 
-interface A {
-	__type?: 'a'
-	bar: 'baz'
-}
+// interface A {
+// 	__type?: 'a'
+// 	bar: 'baz'
+// }
 
-const a: A = {
-	__type: 'a',
-	bar: 'baz',
-}
+// const a: A = {
+// 	__type: 'a',
+// 	bar: 'baz',
+// }
 
-const isA = isType(a, 'a')
-if (isA) {
-	a //=>
-} else {
-	a //=>
-}
-// console.log(`a is ${isA ? 'A' : 'not A'}`)
+// const isA = isType(a, 'a')
+// if (isA) {
+// 	a //=>
+// } else {
+// 	a //=>
+// }
+// // console.log(`a is ${isA ? 'A' : 'not A'}`)
 
-const b = {
-	__type: 'b',
-	bar: 'baz',
-}
+// const b = {
+// 	__type: 'b',
+// 	bar: 'baz',
+// }
 
-const isB = isType(b, 'a')
-if (isB) {
-	b //=>
-} else {
-	b //=>
-}
-// console.log(`b is ${isB ? 'A' : 'not A'}`)
+// const isB = isType(b, 'a')
+// if (isB) {
+// 	b //=>
+// } else {
+// 	b //=>
+// }
+// // console.log(`b is ${isB ? 'A' : 'not A'}`)
 
-const number = 1 as any
-const isNumber = isType(number, 'number')
+// const number = 1 as any
+// const isNumber = isType(number, 'number')
 
-if (isNumber) {
-	number //=>
-} else {
-	number //=>
-}
-// console.log(`number is ${isNumber ? 'number' : 'not number'}`)
+// if (isNumber) {
+// 	number //=>
+// } else {
+// 	number //=>
+// }
+// // console.log(`number is ${isNumber ? 'number' : 'not number'}`)
 
-const string = 'string' as any
-const isString = isType(string, 'string')
-if (isString) {
-	string //=>
-} else {
-	string //=>
-}
-// console.log(`string is ${isString ? 'string' : 'not string'}`)
+// const string = 'string' as any
+// const isString = isType(string, 'string')
+// if (isString) {
+// 	string //=>
+// } else {
+// 	string //=>
+// }
+// // console.log(`string is ${isString ? 'string' : 'not string'}`)
 
-const boolean = true as any
-const isBoolean = isType(boolean, 'boolean')
-if (isBoolean) {
-	boolean //=>
-} else {
-	boolean //=>
-}
-// console.log(`boolean is ${isBoolean ? 'boolean' : 'not boolean'}`)
+// const boolean = true as any
+// const isBoolean = isType(boolean, 'boolean')
+// if (isBoolean) {
+// 	boolean //=>
+// } else {
+// 	boolean //=>
+// }
+// // console.log(`boolean is ${isBoolean ? 'boolean' : 'not boolean'}`)
 
-const func = (() => {}) as any
-const isFunction = isType(func, 'function')
-if (isFunction) {
-	func //=>
-} else {
-	func //=>
-}
-// console.log(`func is ${isFunction ? 'function' : 'not function'}`)
+// const func = (() => {}) as any
+// const isFunction = isType(func, 'function')
+// if (isFunction) {
+// 	func //=>
+// } else {
+// 	func //=>
+// }
+// // console.log(`func is ${isFunction ? 'function' : 'not function'}`)
 
-const obj = {} as any
-const isObject = isType(obj, 'object')
-if (isObject) {
-	obj //=>
-} else {
-	obj //=>
-}
-// console.log(`object is ${isObject ? 'object' : 'not object'}`)
+// const obj = {} as any
+// const isObject = isType(obj, 'object')
+// if (isObject) {
+// 	obj //=>
+// } else {
+// 	obj //=>
+// }
+// // console.log(`object is ${isObject ? 'object' : 'not object'}`)
