a8291ec9ddb14cc5a67f239fafec12759b41c118
Braden Wiggins <braden@fractal-hq.com>
2024-05-24 19:07:43 -0400

    fix: bad doc

--- src/lib/internal/cancelClassFound.ts
+++ src/lib/internal/cancelClassFound.ts
@@ -1,5 +1,5 @@
 /**
- * Checks if an event's composed path contains an element with the `fractils-cancel` class.
+ * Checks if an event's composed path contains an element with the provided classname.
  */
 export function composedPathContains(e: Event, classname: string) {
 	return e.composedPath().some((n) => (n as HTMLElement).classList?.contains(classname))
